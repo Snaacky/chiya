21f890561f0856b10b7fffa1af068634bb3d988a
Snaacky <snaacky@pm.me>
2020-12-13 11:18:54 -0500

    Update config.example.py

--- config.example.py
+++ config.example.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "" 			                                # Bot token stored as string
 OWNER_ID = 00000000000000                           # Owner ID stored as int
-SUBMISSION_CHANNEL_IDs = [00000000000, 0000000000]  # Channels where only link/image submissions are allowed.
+SUBMISSION_CHANNEL_IDs = [00000000000, 0000000000]  # Channels where submissions are limited to links and images stored as list
