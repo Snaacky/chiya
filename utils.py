6138f9c2c0c4e0e57c54607d64b2abbee4fe9c20
stalkerx <SuperstalkerX@gmail.com>
2020-12-17 01:40:16 -0600

    Submission only channel checks (#6)
    * Update config.example.py
    
    * General improvements, work on wallpapers-moe remover, removed excess code.
    
    Purged the project of some unused bloat from previous testing. Simplified contains_link
    and has_attachments and made them more sane. Added embeds.py to store the embeds for future
    use. Added self-destructing warning embed for the wallpapers-moe remover. Made it so the
    remover won't interact with bots.
    
    Co-authored-by: [REDACTED] <vikee.email@gmail.com>
    Co-authored-by: Snaacky <snaacky@pm.me>

--- utils.py
+++ utils.py
@@ -1,22 +1,16 @@
-import discord
 import re
 import config
 
 
-
-async def is_owner(ctx):
-    print(ctx.message.author)
+def is_owner(ctx):
     return ctx.message.author.id == config.OWNER_ID
 
-def contains_link(message):
-    regex = r"(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})"
-    if (len(re.findall(regex, message.content))!=0): #using regular expressions to find if the message contains a link.
-        return True
-    else:
-        return False
-    
-def has_attachment(message):
-    if (message.attachments): #checking if message has any attachments
-        return True
-    
-    return False
+
+def contains_link(ctx):
+    regex = r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
+    result = re.search(regex, ctx.content)
+    return True if result else False
+
+
+def has_attachment(ctx):
+    return True if ctx.attachments else False
