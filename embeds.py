eab019fb39251f27ebae8427fc4bc65d867065c3
stalker <superstalkerX@gmail.com>
2020-12-18 01:21:22 -0600

    added method to make embeds easier

--- embeds.py
+++ embeds.py
@@ -1,8 +1,41 @@
 import discord
 
 
-def files_and_links_only(ctx):
-    embed = discord.Embed(description="This channel is for submissions only! All messages that do not contain an image or a link are automatically removed.")
-    embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
+def make_embed(ctx: discord.ext.commands.Context=None, color='dark_theme', title: str = None, description: str = None,
+          image_url: str = None, author=True) -> discord.Embed:
+    """Global embed template"""
+
+    # This is a list of colors that can be referenced by name rather than by hex value.
+    colors = dict(default=0, teal=0x1abc9c, dark_teal=0x11806a, green=0x2ecc71, dark_green=0x1f8b4c, blue=0x3498db,
+                  dark_blue=0x206694, purple=0x9b59b6, dark_purple=0x71368a, magenta=0xe91e63, dark_magenta=0xad1457,
+                  gold=0xf1c40f, dark_gold=0xc27c0e, orange=0xe67e22, dark_orange=0xa84300, red=0xe74c3c,
+                  dark_red=0x992d22, lighter_grey=0x95a5a6, dark_grey=0x607d8b, light_grey=0x979c9f,
+                  darker_grey=0x546e7a, blurple=0x7289da, greyple=0x99aab5, dark_theme=0x36393F,
+                  # Reddit colors:
+                  reddit=0xff5700, orange_red=0xFF450, upvote=0xFF8b60, neutral=0xC6C6C6, 
+                  downvote=0x9494FF, light_bg=0xEFF7FF, header=0xCEE3F8, ui_text=0x336699)
+
+    # If the color given was a valid name, use the corresponding hex value, else assume the value is already in hex form.
+    if color in colors:
+        embed = discord.Embed(color=colors[color],
+                        title=title, description=description)
+    else:
+        embed = discord.Embed(color=color,
+                        title=title, description=description)
+
+    if author:
+        embed.set_author(icon_url=ctx.author.avatar_url,
+                    name=str(ctx.author))
+
+    if image_url:
+        embed.set_thumbnail(url=image_url)
+
+    embed.timestamp = ctx.created_at
+
+    return embed
+
+
+def files_and_links_only(ctx: discord.ext.commands.Context) -> discord.Embed:
+    embed = make_embed(ctx, description="This channel is for submissions only! All messages that do not contain an image or a link are automatically removed.", color="reddit")
     embed.set_footer(text="This message will self-destruct in 10 seconds.")
     return embed
