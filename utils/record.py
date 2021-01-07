2d7e1c5a5d2dce0ebaca7a59b8ab71c244e78c07
stalker <superstalkerX@gmail.com>
2021-01-06 20:51:59 -0600

    removed time from useage logger, redundancy

--- utils/record.py
+++ utils/record.py
@@ -6,4 +6,4 @@ log = logging.getLogger(__name__)
 
 async def record_usage(self, ctx):
     """Recording useage of command"""
-    log.info(msg=f"{ctx.author} issued command {ctx.command} {ctx.message.created_at} @ {ctx.guild} in {ctx.channel}")
+    log.info(msg=f"{ctx.author} issued command {ctx.command} @ {ctx.guild} in {ctx.channel}")
