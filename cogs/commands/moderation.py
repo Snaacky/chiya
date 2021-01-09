44b14af035d4f8807108065738de48313b4e5042
Snaacky <snaacky@pm.me>
2021-01-09 12:13:55 -0500

    Added database support, further work on commands, updated to discord.py 1.6
    - Updated requirements.txt from discord.py 1.5.1 to 1.6.0.
    - Removed random \n after discord.py version.
    - Implemented utils/database.py for general DB functions.
    - Implemented error-checking, DB logging, and made `ban` actually work.
    - Implemented error-checking and DB logging for `unban`.
    - Implemented error-checking and DB logging for `kick`.
    - Added "Discord Mod" permission requirements to all moderation actions.
    - Added is_owner() permission requirement to `rules` generation.
    - Implemented `warn` command.
    - Sorted imports into Python, 3rd party, and local imports across files.
    - Adjusted permissions on utilities cog to "Discord Mod" or is_owner() where necessary.

--- cogs/commands/moderation.py
+++ cogs/commands/moderation.py
@@ -1,14 +1,16 @@
-"""
-This File is for demonstrating and used as a template for future cogs.
-"""
-
+import itertools
 import logging
-import discord
+import os
+import time
 
+import dataset
+import discord
 from discord.ext import commands
+
+import config
+import utils.database
 from utils import embeds
 from utils.record import record_usage
-
 
 # Enabling logs
 log = logging.getLogger(__name__)
@@ -26,21 +28,41 @@ class ModerationCog(commands.Cog):
     async def ban_member(self, ctx, user, reason: str, delete_message_days: int = 0):
         """ Bans user from guild """
         member = await commands.converter.UserConverter().convert(ctx, user)
-        
+
         if member is None:
             raise commands.UserNotFound(user)
-        
+
+        # Stop user from banning themselves.
         if ctx.author.id == member.id:
-            await ctx.send("are you retarded")
-            return  # TODO: Implement error embed here
+            await ctx.reply("You cannot ban yourself!")
+            return  # TODO: Implement error embed here.
+
+        # Stop user from trying to ban Chiya (not that it's possible).
+        if self.bot.user.id == member.id:
+            await ctx.reply("... Excuse me?")
+            return  # TODO: Implement error embed here.
+
+        # For some reason, Discord lets you .ban() an already banned person without error.
+        try:
+            ban = await ctx.guild.fetch_ban(member)
+            if ban:
+                await ctx.reply("That user is already banned!")
+                return  # TODO: Implement error embed here.
+        except discord.errors.NotFound:
+            pass
 
         # Info: https://discordpy.readthedocs.io/en/stable/api.html#discord.Guild.ban
-        await ctx.guild.ban(member, delete_message_days, reason)
-        await ctx.send(f"Banned {member} by {ctx.author} for {reason}")
+        await ctx.guild.ban(user=member, reason=reason, delete_message_days=delete_message_days)
 
-        # TODO: Log to database.
+        # Add the ban to the mod_log database.
+        with dataset.connect(utils.database.get_db()) as tx:
+            tx["mod_logs"].insert(dict(
+                user_id=member.id, mod_id=ctx.author.id, timestamp=int(time.time()), reason=reason, type="ban"
+            ))
+
+        # TODO: Add DM letting the user know they were banned and the reason.
+        await ctx.reply(f"Banned {member} for {reason}")
         # TODO: Return successfully banned user embed.
-        return NotImplementedError
 
     @commands.has_role("Discord Mod")
     @commands.before_invoke(record_usage)
@@ -51,71 +73,132 @@ class ModerationCog(commands.Cog):
 
         if member is None:
             raise commands.UserNotFound(user)
-        
+
         if ctx.author.id == member.id:
-            await ctx.send("are you retarded")
-            return  # TODO: Implement error embed here
+            await ctx.reply("You cannot unban yourself!")
+            return  # TODO: Implement error embed here.
+
+        # Check to see if the user is actually banned.
+        try:
+            await ctx.guild.fetch_ban(member)
+        except discord.errors.NotFound:
+            await ctx.reply("Unable to find ban for that user!")
+            return  # TODO: Implement error embed here.
 
         # Info: https://discordpy.readthedocs.io/en/stable/api.html#discord.Guild.unban
         await ctx.guild.unban(user=member, reason=reason)
-        await ctx.send(f"Unbanned {member} by {ctx.author} for {reason}")
-        # TODO: Log to database.
-        # TODO: Return successfully unbanned user embed.
-        return NotImplementedError
+
+        # Add the unban to the mod_log database.
+        with dataset.connect(utils.database.get_db()) as tx:
+            tx["mod_logs"].insert(dict(
+                user_id=member.id, mod_id=ctx.author.id, timestamp=int(time.time()), reason=reason, type="unban"
+            ))
+
+        await ctx.reply(f"Unbanned {member} for {reason}")
 
     @commands.has_role("Discord Mod")
     @commands.before_invoke(record_usage)
     @commands.command(name="kick")
     async def kick_member(self, ctx, user, reason: str):
-        """Kicks user from guild."""
+        """ Kicks user from guild. """
         member = await commands.converter.UserConverter().convert(ctx, user)
         if member is None:
             raise commands.UserNotFound(user)
-        
-        if ctx.author.id == member.id:
-            await ctx.send("are you retarded")
-            return  # TODO: Implement error embed here
-            
 
-        if member.roles in await commands.converter.RoleConverter().convert(ctx, "Discord Mod"):
-            # does this even work?
-            await ctx.send("This works!")
-        else:
-            await ctx.send("doesn't work. 😢")
+        if ctx.author.id == member.id:
+            await ctx.reply("You cannot unban yourself!")
+            return  # TODO: Implement error embed here
+
+        # Stop user from trying to kick Chiya (not that it's possible).
+        if self.bot.user.id == member.id:
+            await ctx.reply("... Excuse me?")
+            return  # TODO: Implement error embed here.
+
+        if ctx.guild.fetch_member(member) is None:
+            await ctx.reply("That user is not in the server.")
+            return  # TODO: Implement error embed here.
 
         # Info: https://discordpy.readthedocs.io/en/stable/api.html#discord.Guild.kick
         await ctx.guild.kick(user=member, reason=reason)
-        await ctx.send(f"Kicked {member} by {ctx.author} for {reason}")
-        # TODO: Log to database.
+
+        # Add the kick to the mod_log database.
+        with dataset.connect(utils.database.get_db()) as tx:
+            tx["mod_logs"].insert(dict(
+                user_id=member.id, mod_id=ctx.author.id, timestamp=int(time.time()), reason=reason, type="kick"
+            ))
+        
+        # Respond to the context that the user was kicked.
+        await ctx.reply(f"Kicked {member} for {reason}")
         # TODO: Return successfully kicked user embed.
+        # TODO: Add DM letting the user know they were banned and the reason.
         return NotImplementedError
 
+    @commands.has_role("Discord Mod")
     @commands.before_invoke(record_usage)
     @commands.command(name="mute")
     async def mute(self, ctx):
         return NotImplementedError
-    
+
+    @commands.has_role("Discord Mod")
     @commands.before_invoke(record_usage)
     @commands.command(name="unmute")
     async def unmute(self, ctx):
         return NotImplementedError
 
+    @commands.has_role("Discord Mod")
     @commands.before_invoke(record_usage)
     @commands.command(name="warn")
-    async def warn(self, ctx):
-        return NotImplementedError
+    async def warn(self, ctx, user, *reason: str):
+        """ Kicks user from guild. """
+        member = await commands.converter.UserConverter().convert(ctx, user)
+        if member is None:
+            raise commands.UserNotFound(user)
+        
+        # TODO: Uncomment this, temporarily disabled for testing!
+        # if ctx.author.id == member.id:
+        #    await ctx.reply("You cannot warn yourself!")
+        #    return  # TODO: Implement error embed here
 
+        if ctx.guild.fetch_member(member) is None:
+            await ctx.reply("That user is not in the server.")
+            return  # TODO: Implement error embed here.
+
+        # Converts reason from a list of strings into a full sentence string.
+        reason = " ".join(reason)
+        
+        # Attempts to creates the DM channel in case the bot hasn't interacted with the user before.
+        channel = await member.create_dm()
+        await channel.send(f"""
+        You received a warning in {ctx.guild.name}!
+        
+Reason: {reason}
+
+You do not have to acknowledge or respond to this warning but please keep in mind that contentious bad behavior will result in more severe punishment in the future.
+""")  # TODO: Replace with an embed!
+
+        # Add the warning to the mod_log database.
+        with dataset.connect(utils.database.get_db()) as tx:
+            tx["mod_logs"].insert(dict(
+                user_id=member.id, mod_id=ctx.author.id, timestamp=int(time.time()), reason=reason, type="warn"
+            ))
+        
+        # Respond to the context that the user was kicked.
+        await ctx.reply(f"Warned {member} for {reason}")
+        # TODO: Return successfully kicked user embed.
+
+    @commands.has_role("Discord Mod")
     @commands.before_invoke(record_usage)
     @commands.command(name="notes")
     async def notes(self, ctx):
         return NotImplementedError
-    
+
+    @commands.has_role("Discord Mod")
     @commands.before_invoke(record_usage)
     @commands.command(name="addnote")
     async def add_note(self, ctx):
         return NotImplementedError
-    
 
+    @commands.is_owner()
     @commands.before_invoke(record_usage)
     @commands.command(name="rules")
     async def rules(self, ctx):
