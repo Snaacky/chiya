a5aac01215af1f559c8712628c13553ae3e5e397
Snaacky <Snaacky@pm.me>
2021-04-03 12:47:14 -0400

    Re-did rules command, added new colors to make_embed

--- cogs/commands/administration.py
+++ cogs/commands/administration.py
@@ -23,47 +23,36 @@ class AdministrationCog(Cog):
     async def rules(self, ctx: Context):
         """ Generates the #rules channel embeds. """
 
-        embed = discord.Embed(colour=discord.Colour(0x2f3136))
+        embed = embeds.make_embed(color="quotes_grey")
         embed.set_image(url="https://i.imgur.com/Yk4kwZy.gif")
         await ctx.send(embed=embed)
 
-        embed = discord.Embed(
-            title="Rule #1: Do not send copyright infringing content.",
-            description="Posting copyright infringing material including streams, torrents, downloads, or Discord file uploads are forbidden by Discord's terms of service and will be removed.")
+        embed = embeds.make_embed(title="📃  Discord Server Rules", color="quotes_grey", description="This list is not all-encompassing and you may be actioned for a reason outside of these rules. Use common sense when interacting in our community.")
+        embed.add_field(name="Rule 1: Do not send copyright-infringing material.", inline=False, value="> Linking to copyright-infringing content via torrents, pirated stream links, direct download links, or uploading copyright-infringing material over Discord puts our community at risk of being shut down. We are a discussion community, not a file-sharing hub. ")
+        embed.add_field(name="Rule 2: Be courteous and mindful of others.", inline=False, value="> Do not engage in toxic behavior such as spamming, baiting, derailing conversations, attacking or mocking other users, doxxing, or attempting to instigate drama. Bigotry and hate speech will not be tolerated. Do not use offensive or problematic profile pictures, usernames, nicknames.")
+        embed.add_field(name="Rule 3: Do not post self-promotional content.", inline=False, value="> We are not a billboard and are certainly not the place to advertise your Discord server, app, website, service, etc.")
+        embed.add_field(name="Rule 4: Do not post unmarked spoilers.", inline=False, value="> Use spoiler tags and include what series or episode your spoiler is in reference to outside the spoiler tag so people don't blindly click a spoiler.")
+        embed.add_field(name="Rule 5: Do not backseat moderate.", inline=False, value="> If you see someone breaking the rules or have something to report, please contact staff. Do not take matters into your own hands and respect the decision of the staff.")
+        embed.add_field(name="Rule 6: Do not abuse pings.", inline=False, value="> Do not ping staff outside of conversation unless necessary. Do not ping VIP users for questions or help with their service. Do not spam or ghost ping other users.")
+        embed.add_field(name="Rule 7: Do not beg, buy, sell, or trade.", inline=False, value="> This includes, but is not limited to, server ranks, roles, permissions, giveaways, private community invites, or any digital or physical goods.")
         await ctx.send(embed=embed)
 
-        embed = discord.Embed(
-            title="Rule #2: Do not harass other users.",
-            description="Please be courteous and respectful to your fellow server members. We will not tolerate harassment or personal attacks in our community.")
+        # Inlines are 3 columns, this hack breaks our 3 columns down into 2 because the third doesn't render.
+        embed = embeds.make_embed(title="🔗  Our Links", color="quotes_grey")
+        embed.add_field(name="Reddit:", inline=True, value="> [/r/animepiracy](https://reddit.com/r/animepiracy)")
+        embed.add_field(name="Discord:", inline=True, value="> [discord.gg/piracy](https://discord.gg/piracy)")
+        embed.add_field(name="Index:", inline=True, value="> [piracy.moe](https://piracy.moe)")
+        embed.add_field(name="Wiki:", inline=True, value="> [wiki.piracy.moe](https://wiki.piracy.moe)")
+        embed.add_field(name="Seadex:", inline=True, value="> [seadex.piracy.moe](https://seadex.piracy.moe)")
+        embed.add_field(name="GitHub:", inline=True, value="> [github.com/ranimepiracy](https://github.com/ranimepiracy)")
+        embed.add_field(name="Twitter:", inline=True, value="> [@ranimepiracy](https://twitter.com/ranimepiracy)")
+        embed.add_field(name="Uptime Status:", inline=True, value="> [status.piracy.moe](https://status.piracy.moe/)")
         await ctx.send(embed=embed)
 
-        embed = discord.Embed(
-            title="Rule #3: Do not spam.",
-            description="Spamming is strictly forbidden. This includes but is not limited to: large walls of text, copypasta, and/or repetitive message spam.")
-        await ctx.send(embed=embed)
+        
 
-        embed = discord.Embed(
-            title="Rule #4: Do not send NSFW content outside of NSFW channels.",
-            description="NSFW content is only allowed in channels marked as NSFW. You can opt-in to our NSFW channel through the #gate. NSFL content is not allowed.")
-        await ctx.send(embed=embed)
-
-        embed = discord.Embed(
-            title="Rule #5: Do not post unmarked spoilers.",
-            description="All spoilers or potential spoilers must be marked as a spoiler. Instructions on how to use Discord spoilers can be found [here](https://support.discord.com/hc/en-us/articles/360022320632-Spoiler-Tags-).")
-        await ctx.send(embed=embed)
-
-        embed = discord.Embed(
-            title="Rule #6: Do not advertise without permission.",
-            description="All promotional content must be approved beforehand by the mod team. This includes websites, apps, social media, etc. We do not allow advertising through DMs under any circumstances.")
-        await ctx.send(embed=embed)
-
-        embed = discord.Embed(
-            title="Rule #7: Do not request or B/S/T invites.",
-            description="Do not request, buy, sell, trade, or publicly give away invites for private communities through our server. Any of the above actions is a bannable offense in most private communities.")
-        await ctx.send(embed=embed)
-
-        await ctx.send("https://discord.gg/piracy")
-        await ctx.send("https://piracy.moe")
+        #await ctx.send("https://discord.gg/piracy")
+        # await ctx.send("https://piracy.moe")
 
 
 def setup(bot: Bot) -> None:
--- utils/embeds.py
+++ utils/embeds.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext.commands import Context
 
 
-def make_embed(title: str = None, description: str = None, context: Context = None, color='dark_theme',
+def make_embed(title: str = None, description: str = None, context: Context = None, color ='dark_theme',
           image_url: str = None, author=True) -> discord.Embed:
     """General embed template
 
@@ -19,11 +19,12 @@ def make_embed(title: str = None, description: str = None, context: Context = No
     """
 
     # This is a list of colors that can be referenced by name rather than by hex value.
-    colors = dict(default=0, teal=0x1abc9c, dark_teal=0x11806a, green=0x2ecc71, dark_green=0x1f8b4c, blue=0x3498db,
+    colors = dict(default=0x202225, black=0, teal=0x1abc9c, dark_teal=0x11806a, green=0x2ecc71, dark_green=0x1f8b4c, blue=0x3498db,
                   dark_blue=0x206694, purple=0x9b59b6, dark_purple=0x71368a, magenta=0xe91e63, dark_magenta=0xad1457,
                   gold=0xf1c40f, dark_gold=0xc27c0e, orange=0xe67e22, dark_orange=0xa84300, red=0xe74c3c,
                   dark_red=0x992d22, lighter_grey=0x95a5a6, dark_grey=0x607d8b, light_grey=0x979c9f,
-                  darker_grey=0x546e7a, blurple=0x7289da, greyple=0x99aab5, dark_theme=0x36393F, nitro_pink=0xff73fa,
+                  darker_grey=0x546e7a, blurple=0x7289da, greyple=0x99aab5, dark_theme=0x36393F, nitro_pink=0xff73fa, 
+                  blank=0x2f3136, quotes_grey=0x4f545c,
                   # Reddit colors:
                   reddit=0xff5700, orange_red=0xFF450, upvote=0xFF8b60, neutral=0xC6C6C6, 
                   downvote=0x9494FF, light_bg=0xEFF7FF, header=0xCEE3F8, ui_text=0x336699)
