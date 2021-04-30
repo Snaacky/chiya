c1ab727ca038af8cbaa20b847ae496eef96d899f
Snaacky <Snaacky@pm.me>
2021-04-30 14:38:39 -0400

    user.mention doesn't need to be in a f-string

--- cogs/commands/moderation/boosters.py
+++ cogs/commands/moderation/boosters.py
@@ -18,7 +18,7 @@ class BoostersCog(Cog):
 
         embed = embeds.make_embed(ctx=ctx, title=f"Total boosts: {ctx.guild.premium_subscription_count}", 
             image_url=config.nitro, color="nitro_pink", author=False)
-        description = "\n".join(f"{user.mention}" for user in ctx.guild.premium_subscribers)
+        description = "\n".join(user.mention for user in ctx.guild.premium_subscribers)
         embed.set_footer(text=f"Total boosters: {len(ctx.guild.premium_subscribers)}")
         embed.description=description
         await ctx.reply(embed=embed)
