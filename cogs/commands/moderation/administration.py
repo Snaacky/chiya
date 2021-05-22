422ce72dc19b26a1770b83fe901d0afc32bb3823
lunarmint <luunhatminh1999@icloud.com>
2021-05-22 05:07:51 -0400

    Original administration.py
    Accidentally commited the test version of administration.py

--- cogs/commands/moderation/administration.py
+++ cogs/commands/moderation/administration.py
@@ -17,6 +17,7 @@ from utils.record import record_usage
 # Enabling logs
 log = logging.getLogger(__name__)
 
+
 class AdministrationCog(Cog):
     """ Administration Cog Cog """
 
@@ -52,7 +53,7 @@ class AdministrationCog(Cog):
     async def say(self, ctx, *, args):
         """Echos the input argument."""
         await ctx.send(args)
-    
+
     @commands.is_owner()
     @utilities.command(name="eval")
     async def eval(self, ctx, *, body: str):
@@ -135,15 +136,15 @@ class AdministrationCog(Cog):
             cog = re.search(regex, str(ctx.bot.cogs[name_of_cog]))
             try:
                 self.bot.reload_extension(cog.group())
-                
+
             except commands.ExtensionError as e:
                 await ctx.message.add_reaction("❌")
                 await ctx.send(f'{e.__class__.__name__}: {e}')
-            
+
             else:
                 await ctx.message.add_reaction("✔")
                 await ctx.send(f"Reloaded `{cog.group()}` module!")
-        
+
         elif name_of_cog is None:
             # Reload all the cogs in the folder named cogs.
             # Skips over any cogs that start with '__' or do not end with .py.
@@ -165,7 +166,7 @@ class AdministrationCog(Cog):
             await ctx.message.add_reaction("❌")
             await ctx.send("Module not found, check spelling, it's case sensitive.")
 
-    @commands.is_owner()
+    @commands.has_role(config.role_admin)
     @commands.bot_has_permissions(embed_links=True, send_messages=True)
     @commands.before_invoke(record_usage)
     @commands.command(name="rules")
@@ -178,15 +179,24 @@ class AdministrationCog(Cog):
         await ctx.send(embed=embed)
 
         # The actual rules embed
-        embed = embeds.make_embed(title="📃  Discord Server Rules", color="quotes_grey", description="This list is not all-encompassing and you may be actioned for a reason outside of these rules. Use common sense when interacting in our community.")
-        embed.add_field(name="Rule 1: Do not send copyright-infringing material.", inline=False, value="> Linking to torrents, pirated stream links, direct download links, or uploading files over Discord puts our community at risk of being shut down. We are a discussion community, not a file-sharing hub.")
-        embed.add_field(name="Rule 2: Be courteous and mindful of others.", inline=False, value="> Do not engage in toxic behavior such as spamming, derailing conversations, attacking other users, or attempting to instigate drama. Bigotry will not be tolerated. Avoid problematic avatars, usernames, or nicknames.")
-        embed.add_field(name="Rule 3: Do not post self-promotional content.", inline=False, value="> We are not a billboard nor the place to advertise your Discord server, app, website, service, etc.")
-        embed.add_field(name="Rule 4: Do not post unmarked spoilers.", inline=False, value="> Use spoiler tags and include what series or episode your spoiler is in reference to outside the spoiler tag so people don't blindly click a spoiler.")
-        embed.add_field(name="Rule 5: Do not backseat moderate.", inline=False, value="> If you see someone breaking the rules or have something to report, please submit a <#829861810999132160> ticket.")
-        embed.add_field(name="Rule 6: Do not abuse pings.", inline=False, value="> Do not ping staff outside of conversation unless necessary. Do not ping VIP users for questions or help with their service. Do not spam or ghost ping other users.")
-        embed.add_field(name="Rule 7: Do not beg, buy, sell, or trade.", inline=False, value="> This includes, but is not limited to, server ranks, roles, permissions, giveaways, private community invites, or any digital or physical goods.")
-        embed.add_field(name="Rule 8: Follow the Discord Community Guidelines and Terms of Service.", inline=False, value="> The Discord Community Guidelines and Terms of Service govern all servers on the platform. Please familarize yourself with them and the restrictions that come with them. \n> \n> https://discord.com/guidelines \n> https://discord.com/terms")
+        embed = embeds.make_embed(title="📃  Discord Server Rules", color="quotes_grey",
+                                  description="This list is not all-encompassing and you may be actioned for a reason outside of these rules. Use common sense when interacting in our community.")
+        embed.add_field(name="Rule 1: Do not send copyright-infringing material.", inline=False,
+                        value="> Linking to torrents, pirated stream links, direct download links, or uploading files over Discord puts our community at risk of being shut down. We are a discussion community, not a file-sharing hub.")
+        embed.add_field(name="Rule 2: Be courteous and mindful of others.", inline=False,
+                        value="> Do not engage in toxic behavior such as spamming, derailing conversations, attacking other users, or attempting to instigate drama. Bigotry will not be tolerated. Avoid problematic avatars, usernames, or nicknames.")
+        embed.add_field(name="Rule 3: Do not post self-promotional content.", inline=False,
+                        value="> We are not a billboard nor the place to advertise your Discord server, app, website, service, etc.")
+        embed.add_field(name="Rule 4: Do not post unmarked spoilers.", inline=False,
+                        value="> Use spoiler tags and include what series or episode your spoiler is in reference to outside the spoiler tag so people don't blindly click a spoiler.")
+        embed.add_field(name="Rule 5: Do not backseat moderate.", inline=False,
+                        value="> If you see someone breaking the rules or have something to report, please submit a <#829861810999132160> ticket.")
+        embed.add_field(name="Rule 6: Do not abuse pings.", inline=False,
+                        value="> Do not ping staff outside of conversation unless necessary. Do not ping VIP users for questions or help with their service. Do not spam or ghost ping other users.")
+        embed.add_field(name="Rule 7: Do not beg, buy, sell, or trade.", inline=False,
+                        value="> This includes, but is not limited to, server ranks, roles, permissions, giveaways, private community invites, or any digital or physical goods.")
+        embed.add_field(name="Rule 8: Follow the Discord Community Guidelines and Terms of Service.", inline=False,
+                        value="> The Discord Community Guidelines and Terms of Service govern all servers on the platform. Please familarize yourself with them and the restrictions that come with them. \n> \n> https://discord.com/guidelines \n> https://discord.com/terms")
         await ctx.send(embed=embed)
 
         # /r/animepiracy links embed
@@ -196,7 +206,8 @@ class AdministrationCog(Cog):
         embed.add_field(name="Index:", inline=True, value="> [piracy.moe](https://piracy.moe)")
         embed.add_field(name="Wiki:", inline=True, value="> [wiki.piracy.moe](https://wiki.piracy.moe)")
         embed.add_field(name="Seadex:", inline=True, value="> [seadex.piracy.moe](https://seadex.piracy.moe)")
-        embed.add_field(name="GitHub:", inline=True, value="> [github.com/ranimepiracy](https://github.com/ranimepiracy)")
+        embed.add_field(name="GitHub:", inline=True,
+                        value="> [github.com/ranimepiracy](https://github.com/ranimepiracy)")
         embed.add_field(name="Twitter:", inline=True, value="> [@ranimepiracy](https://twitter.com/ranimepiracy)")
         embed.add_field(name="Uptime Status:", inline=True, value="> [status.piracy.moe](https://status.piracy.moe/)")
         await ctx.send(embed=embed)
@@ -204,31 +215,32 @@ class AdministrationCog(Cog):
         # Clean up the command invoker
         await ctx.message.delete()
 
-    @commands.is_owner()
+    @commands.has_role(config.role_admin)
     @commands.bot_has_permissions(embed_links=True, send_messages=True)
     @commands.before_invoke(record_usage)
     @commands.command(name="createticketembed")
     async def create_ticket_embed(self, ctx: Context):
-        embed = embeds.make_embed(title="🎫 Create a new modmail ticket", 
-                                  description="Click the react below to create a new modmail ticket.", 
+        embed = embeds.make_embed(title="🎫 Create a new modmail ticket",
+                                  description="Click the react below to create a new modmail ticket.",
                                   color="default")
         embed.add_field(name="Warning:", value="Serious inquiries only. Abuse may result in warning or ban.")
         spawned = await ctx.send(embed=embed)
         await spawned.add_reaction("🎫")
         await ctx.message.delete()
 
-    @commands.is_owner()
+    @commands.has_role(config.role_admin)
     @commands.bot_has_permissions(embed_links=True, send_messages=True)
     @commands.before_invoke(record_usage)
     @commands.command(name="createcolorrolesembed", aliases=['ccre'])
     async def create_color_roles_embed(self, ctx: Context):
-        embed = discord.Embed(description=f"You can react to one of the squares below to be assigned a colored user role. If you are interested in a different color, you can become a <@&{config.role_server_booster}> to receive a custom colored role.")
+        embed = discord.Embed(
+            description=f"You can react to one of the squares below to be assigned a colored user role. If you are interested in a different color, you can become a <@&{config.role_server_booster}> to receive a custom colored role.")
         msg = await ctx.send(embed=embed)
 
         # API call to fetch all the emojis to cache, so that they work in future calls
         emotes_guild = await ctx.bot.fetch_guild(config.emoji_guild_id)
         emojis = await emotes_guild.fetch_emojis()
-        
+
         await msg.add_reaction(":redsquare:805032092907601952")
         await msg.add_reaction(":orangesquare:805032107952308235")
         await msg.add_reaction(":yellowsquare:805032120971165709")
@@ -238,14 +250,14 @@ class AdministrationCog(Cog):
         await msg.add_reaction(":purplesquare:805032172074696744")
         await ctx.message.delete()
 
-    @commands.is_owner()
+    @commands.has_role(config.role_admin)
     @commands.bot_has_permissions(embed_links=True, send_messages=True)
     @commands.before_invoke(record_usage)
     @commands.command(name="createassignablerolesembed", aliases=['care'])
     async def create_assignable_roles_embed(self, ctx: Context):
         role_assignment_text = """
         You can react to one of the emotes below to assign yourself an event role.
-        
+
         🎁  <@&832528733763928094> - Receive giveaway pings.
         📢  <@&827611682917711952> - Receive server announcement pings.
         📽  <@&831999443220955136> - Receive group watch event pings.
@@ -259,7 +271,7 @@ class AdministrationCog(Cog):
         # API call to fetch all the emojis to cache, so that they work in future calls
         emotes_guild = await ctx.bot.fetch_guild(config.emoji_guild_id)
         await emotes_guild.fetch_emojis()
-        
+
         await msg.add_reaction("🎁")
         await msg.add_reaction("📢")
         await msg.add_reaction("📽")
