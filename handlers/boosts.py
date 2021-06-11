e84ff007d19467d7bf6fc90080a6f56e16bc25c4
Snaacky <snaacky@pm.me>
2021-06-10 22:26:34 -0400

    Added docstrings for sanity and hopefully fixed boost related bugs

--- handlers/boosts.py
+++ handlers/boosts.py
@@ -8,8 +8,19 @@ from utils import embeds
 log = logging.getLogger(__name__)
 
 async def on_new_boost(before, after):
+    """ 
+    Process whenever a new boost is received.
+
+    Parameters:
+        before (discord.Guild): The guild prior to being updated.
+        after  (discord.Guild): The guild after being updated.
+        
+    Called from:
+        cogs/listeners/guild_updates.py: on_guild_update()
+        
+    """
+    # TODO: Replace hardcoded mention with a .mention from the config file.
     if after.premium_subscription_count > before.premium_subscription_count:
-        """ Process whenver a new boost is received, regardless of previous boost status. """
         # Send an embed in the system channel thanking the user for boosting.
         embed = embeds.make_embed(author=False, color="nitro_pink")
         embed.title = f"A new booster appeared!"
@@ -18,48 +29,78 @@ async def on_new_boost(before, after):
         embed.set_image(url="https://i.imgur.com/O8R98p9.gif")
         await before.system_channel.send(embed=embed)
 
-        # Sent a log in #nitro-logs letting the staff know someone boosted with a link to a message near the boost.
-        nitro_logs = discord.utils.get(after.guild.channels, id=config.nitro_logs)
+        # Send a embed in #nitro-logs that someone boosted with a link to a message near the boost.
+        nitro_logs = discord.utils.get(after.channels, id=config.nitro_logs)
         embed = embeds.make_embed(author=False, color="nitro_pink")
-        last_message = await after.guild.system_channel.fetch_message(after.guild.system_channel.last_message_id)
+        last_message = await after.system_channel.fetch_message(after.system_channel.last_message_id)
         embed.description(f"[A new boost was added to the server.](https://canary.discord.com/channels/{after.guild.id}/{after.guild.system_channel.id}/{last_message.id})")
         await nitro_logs.send(embed=embed)
 
         # Log the boost to the console.
-        log.info(f"A new boost was added to {after.guild.name}.")
+        log.info(f"A new boost was added to {after.name}.")
 
 async def on_removed_boost(before, after):
-    """ Process whenver a new boost is removed, regardless of previous boost status. """
+    """ 
+    Process whenever a new boost is removed.
+
+    Parameters:
+        before (discord.Guild): The guild prior to being updated.
+        after  (discord.Guild): The guild after being updated.
+    
+    Called from:
+        cogs/listeners/guild_updates.py: on_guild_update()
+    """
     if after.premium_subscription_count < before.premium_subscription_count:
-        # Sent a log in #nitro-logs letting the staff know someone removed a boost.
-        nitro_logs = discord.utils.get(after.guild.channels, id=config.nitro_logs)
+        # Send an embed in #nitro-logs that someone removed a boost.
+        nitro_logs = discord.utils.get(after.channels, id=config.nitro_logs)
         embed = embeds.make_embed(author=False, color="nitro_pink")
-        last_message = await after.guild.system_channel.fetch_message(after.guild.system_channel.last_message_id)
         embed.description(f"A boost was removed from the server.")
         await nitro_logs.send(embed=embed)
 
-        # Log the boost to the console.
-        log.info(f"A boost was removed from {after.guild.name}.")
+        # Log the boost removal to the console.
+        log.info(f"A boost was removed from {after.name}.")
 
 async def process_new_booster(before, after):
-    """ Proces when a user who previously hasn't boosted the server boosts. """
-    if before.premium_since is None and after.premium_since is not None:
-        channel = discord.utils.get(after.guild.channels, id=config.nitro_logs)
+    """ 
+    Process when a user who previously hasn't boosted the server boosts.
+
+    Parameters:
+        before (discord.Member): The updated member’s old info.
+        after  (discord.Member): The updated member’s updated info.
+    
+    Called from:
+        cogs/listeners/member_updates.py: on_member_update()
+    """
+    # Send an embed in #nitro-logs that someone removed a boost.
+    if not before.premium_since and after.premium_since:
+        channel = discord.utils.get(after.channels, id=config.nitro_logs)
         embed = embeds.make_embed(author=False, color="nitro_pink")
         embed.title = "New booster"
-        embed.description = f"""{after.mention} boosted the server. We're now at {after.guild.premium_subscription_count} boosts."""
+        embed.description = f"""{after.mention} boosted the server. We're now at {after.premium_subscription_count} boosts."""
         await channel.send(embed=embed)
 
-        log.info(f'{after.mention} boosted {after.guild.name}.')
+        # Log the boost removal to the console.
+        log.info(f'{after.mention} boosted {after.name}.')
 
 async def process_lost_booster(before, after):
-    """ Process when a user who previously boosted the server removes all of their boosts from the server. """
-    if before.premium_since is not None and after.premium_since is None:
-        channel = discord.utils.get(after.guild.channels, id=config.nitro_logs)
+    """ 
+    Process when a user who previously boosted the server removes all of their boosts from the server.
+
+    Parameters:
+        before (discord.Member): The updated member’s old info.
+        after  (discord.Member): The updated member’s updated info.
+    
+    Called from:
+        cogs/listeners/member_updates.py: on_member_update()
+    """
+    # Send an embed in #nitro-logs that someone stopped boosting the server.
+    if before.premium_since and not after.premium_since:
+        channel = discord.utils.get(after.channels, id=config.nitro_logs)
         embed = embeds.make_embed(author=False, color="nitro_pink")
         embed.title = "Lost booster"
-        embed.description = f"""{after.mention} no longer boosts the server. We're now at {after.guild.premium_subscription_count} boosts."""
+        embed.description = f"""{after.mention} no longer boosts the server. We're now at {after.premium_subscription_count} boosts."""
         await channel.send(embed=embed)
 
-        log.info(f'{after.mention} stopped boosting {after.guild.name}.')
+        # Log the booster removal to the console.
+        log.info(f'{after.mention} stopped boosting {after.name}.')
 
