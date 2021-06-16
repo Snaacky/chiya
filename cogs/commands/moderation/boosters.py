82b21c92bf3b173bab0d5a6eaecc6def6f4770c1
Snaacky <snaacky@pm.me>
2021-06-16 17:46:03 -0400

    Migrated boosters command to slashcommands

--- cogs/commands/moderation/boosters.py
+++ cogs/commands/moderation/boosters.py
@@ -1,27 +1,41 @@
 from discord.ext import commands
-from discord.ext.commands import Cog, Bot, Context
+from discord.ext.commands import Cog, Bot
 
 import config
 from utils import embeds
 from utils.record import record_usage
+from discord_slash import cog_ext, SlashContext
+from discord_slash.utils.manage_commands import create_permission
+from discord_slash.model import SlashCommandPermissionType
 
 class BoostersCog(Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.has_role(config.role_staff)
     @commands.bot_has_permissions(send_messages=True)
     @commands.before_invoke(record_usage)
-    @commands.command(name="boosters", aliases=['boosts'])
-    async def send_boosters_list(self, ctx: Context):
+    @cog_ext.cog_slash(
+        name="boosters", 
+        description="List all the current server boosters",
+        default_permission=False,
+        permissions={
+            622243127435984927: [
+                create_permission(763031634379276308, SlashCommandPermissionType.ROLE, True)
+            ]
+        }
+    )
+    async def boosters(self, ctx: SlashContext):
         """ Sends a list of users boosting the server. """
-
-        embed = embeds.make_embed(ctx=ctx, title=f"Total boosts: {ctx.guild.premium_subscription_count}", 
-            image_url=config.nitro, color="nitro_pink", author=False)
-        description = "\n".join(user.mention for user in ctx.guild.premium_subscribers)
+        embed = embeds.make_embed(
+            ctx=ctx, 
+            title=f"Total boosts: {ctx.guild.premium_subscription_count}", 
+            thumbnail_url=config.nitro, 
+            color="nitro_pink", 
+            author=False
+        )
+        embed.description = "\n".join(user.mention for user in ctx.guild.premium_subscribers)
         embed.set_footer(text=f"Total boosters: {len(ctx.guild.premium_subscribers)}")
-        embed.description=description
-        await ctx.reply(embed=embed)
+        await ctx.send(embed=embed)
 
 def setup(bot: Bot) -> None:
     """ Load the BoosterCog cog. """
