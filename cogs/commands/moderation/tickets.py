1a5313efd89a1b4221a609fa9b1f016ae3a5e32a
Mint <developer@lunarmint.dev>
2021-07-06 12:14:22 -0400

    Import clean up

--- cogs/commands/moderation/tickets.py
+++ cogs/commands/moderation/tickets.py
@@ -3,12 +3,12 @@ import time
 
 import dataset
 import discord
+import privatebinapi
 from discord.ext import commands
 from discord.ext.commands import Cog, Bot
 from discord_slash import cog_ext, SlashContext
-from discord_slash.utils.manage_commands import create_option, create_permission
 from discord_slash.model import SlashCommandPermissionType
-import privatebinapi
+from discord_slash.utils.manage_commands import create_option, create_permission
 
 import config
 from utils import database
