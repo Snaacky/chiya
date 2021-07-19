b8d2c4be67e214df7002e7423ace54b03539c5ef
Mint <developer@lunarmint.dev>
2021-07-18 23:51:20 -0400

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit 568ef7588ebd85c68ea0872102a0abc39c78b0d3.

--- cogs/commands/moderation/kicks.py
+++ cogs/commands/moderation/kicks.py
@@ -34,13 +34,13 @@ class KickCog(Cog):
         options=[
             create_option(
                 name="member",
-                description="The member that will be unmuted",
+                description="The member that will be kicked",
                 option_type=6,
                 required=True
             ),
             create_option(
                 name="reason",
-                description="The reason why the member is being unmuted",
+                description="The reason why the member is being kicked",
                 option_type=3,
                 required=False
             ),
