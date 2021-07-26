d2aca6ce17466313ddadd7a7f8d9f72e8bd37f83
Mint <developer@lunarmint.dev>
2021-07-26 06:14:34 -0400

    Read changelog for full description
    Reworked the mute evader autoban logic, as following:
    
    - Now check if they're having a mute channel with their id. If it exists, on member leave, send to gulag.
    
    - Works on both mute and tempmute instead of just tempmute.
    
    - Now adds an unmute entry into the mod_logs to prevent archive_mute_channel() from throwing exception due to non-existent unmuter.
    
    - No longer throwing NoneType exception if the member leaves without being muted due to attempts on deleting the mute channel that does not exist.
    
    - Fixed a lot more stuffs but I can't even recall what they were anymore and it's reworked anyways so probably w/e.

--- cogs/listeners/mutes.py
+++ cogs/listeners/mutes.py
@@ -2,6 +2,7 @@ import logging
 import time
 
 import dataset
+import discord
 from discord import Member
 from discord.ext import commands
 
@@ -23,10 +24,52 @@ class MutesHandler(commands.Cog):
         # Open a connection to the database.
         db = dataset.connect(database.get_db())
 
-        action = db['timed_mod_actions'].find_one(user_id=member.id, is_done=False, action_type='mute')
         guild = member.guild
-        user = await self.bot.fetch_user(member.id)
-        if action:
+        mute_channel = discord.utils.get(guild.channels, name=f"mute-{member.id}")
+
+        if mute_channel:
+            mod_channel = guild.get_channel(config.mod_channel)
+            user = await self.bot.fetch_user(member.id)
+
+            # Add an unmute entry in the database to prevent archive_mute_channel()'s unmuter throwing NoneType() exception.
+            mute_entry = db["mod_logs"].find_one(user_id=member.id, type="mute")
+            # Add an "unmute" entry into the database.
+            if mute_entry:
+                db["mod_logs"].insert(dict(
+                    user_id=user.id,
+                    mod_id=self.bot.user.id,
+                    timestamp=int(time.time()),
+                    reason="Mute evasion.",
+                    type="unmute"
+                ))
+
+            # Update the mute entry in the timed_mod_actions database to prevent issues with task looping.
+            tempmute_entry = db["timed_mod_actions"].find_one(user_id=member.id, is_done=False, action_type="mute")
+            if tempmute_entry:
+                db["timed_mod_actions"].update(dict(id=tempmute_entry["id"], is_done=True), ["id"])
+
+            # Archive the mute channel
+            mutes = self.bot.get_cog("MuteCog")
+            await mutes.archive_mute_channel(
+                ctx=None,
+                user_id=user.id,
+                reason="Mute channel archived after member banned due to mute evasion.",
+                guild=guild,
+
+            )
+
+            # Add the ban to the mod_log database.
+            db["mod_logs"].insert(dict(
+                user_id=user.id,
+                mod_id=self.bot.user.id,
+                timestamp=int(time.time()),
+                reason="Mute evasion.",
+                type="ban"
+            ))
+
+            # Ban the user.
+            await guild.ban(user, reason="Mute evasion.")
+
             # Creating the embed used to alert the moderators that the mute evading member was banned.
             embed = embeds.make_embed(
                 ctx=None,
@@ -35,34 +78,11 @@ class MutesHandler(commands.Cog):
                 thumbnail_url=config.user_ban,
                 color="soft_red"
             )
-
-            channel = guild.get_channel(config.mod_channel)
-            await guild.ban(user, reason="Mute Evasion.")
-
-        # Add the ban to the mod_log database.
-        db["mod_logs"].insert(dict(
-            user_id=user.id,
-            mod_id=self.bot.user.id,
-            timestamp=int(time.time()),
-            reason="Mute Evasion.",
-            type="ban"
-        ))
-
-        # Resolving the mute so that we don't have to deal with it separately.
-        db["timed_mod_actions"].update(dict(id=action["id"], is_done=True), ["id"])
+            await mod_channel.send(embed=embed)
 
         # Commit the changes to the database and close the connection.
         db.commit()
         db.close()
-
-        # Archive the mute channel
-        mutes = self.bot.get_cog("MuteCog")
-        await mutes.archive_mute_channel(
-            user_id=user.id,
-            guild=guild,
-            unmute_reason="Mute channel archived after member banned due to mute evasion."
-        )
-        await channel.send(embed=embed)
             
 
 def setup(bot) -> None:
