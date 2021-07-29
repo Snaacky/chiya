923674023d2ee30b04c89e941dc7d6db06dcf697
[REDACTED] <vikee.email@gmail.com>
2021-07-29 15:47:20 +0530

    Update the automod to use the new style of DB querying.

--- utils/automod.py
+++ utils/automod.py
@@ -7,38 +7,40 @@ from fuzzywuzzy import fuzz
 
 def check_message(message: discord.Message) -> bool:
     """ Checks Messages by calling various other methods. """
-    with dataset.connect(database.get_db()) as db:
-        # querying everything from the database
-        regex_censors = db['censor'].find(censor_type="regex")
-        for censor in regex_censors:
-            # regex checking
-            if(check_regex(message.content, censor['censor_term'])):
-                return True
+    
+    db = dataset.connect(database.get_db())
+    # querying everything from the database
+    regex_censors = db['censor'].find(censor_type="regex")
+    for censor in regex_censors:
+        # regex checking
+        if(check_regex(message.content, censor['censor_term'])):
+            return True
+    
+    exact_censors = db['censor'].find(censor_type="exact")
+    for censor in exact_censors:
+        # regex checking the "exact" censor terms
+        if(check_exact(message.content, censor['censor_term'])):
+            return True
+    
+    substring_censors = db['censor'].find(censor_type="substring")
+    for censor in substring_censors:
+        # regex checking for substrings of the censor terms
+        if(check_substring(message.content, censor['censor_term'])):
+            return True
         
-        exact_censors = db['censor'].find(censor_type="exact")
-        for censor in exact_censors:
-            # regex checking the "exact" censor terms
-            if(check_exact(message.content, censor['censor_term'])):
-                return True
-        
-        substring_censors = db['censor'].find(censor_type="substring")
-        for censor in substring_censors:
-            # regex checking for substrings of the censor terms
-            if(check_substring(message.content, censor['censor_term'])):
-                return True
-            
-        url_censors = db['censor'].find(censor_type="url")
-        for censor in url_censors:
-            # regex checking for a URL matching ones read from the DB
-            if(check_url(message.content, censor['censor_term'])):
-                return True
-        
-        fuzzy_censors = db['censor'].find(censor_type="fuzzy")
-        for censor in fuzzy_censors:
-            # Doing a fuzzy matching with the word, with the specified threshold
-            if(check_fuzzy(message.content, censor['censor_term'], censor['censor_threshold'])):
-                return True
-
+    url_censors = db['censor'].find(censor_type="url")
+    for censor in url_censors:
+        # regex checking for a URL matching ones read from the DB
+        if(check_url(message.content, censor['censor_term'])):
+            return True
+    
+    fuzzy_censors = db['censor'].find(censor_type="fuzzy")
+    for censor in fuzzy_censors:
+        # Doing a fuzzy matching with the word, with the specified threshold
+        if(check_fuzzy(message.content, censor['censor_term'], censor['censor_threshold'])):
+            return True
+    
+    db.close()
     # nothing matched :(
     return False
 
