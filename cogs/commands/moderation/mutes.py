5b52d3d21a23409bad8646aa0b5ac7b76fe42354
Mint <developer@lunarmint.dev>
2021-08-05 13:17:25 -0400

    mutes.py refactor

--- cogs/commands/moderation/mutes.py
+++ cogs/commands/moderation/mutes.py
@@ -11,8 +11,8 @@ from discord_slash import cog_ext, SlashContext
 from discord_slash.model import SlashCommandPermissionType
 from discord_slash.utils.manage_commands import create_option, create_permission
 
-import config
 import utils.duration
+from cogs.commands import settings
 from utils import database
 from utils import embeds
 from utils.moderation import can_action_member
@@ -30,7 +30,7 @@ class MuteCog(Cog):
 
     @staticmethod
     async def mute_member(ctx: SlashContext, member: discord.Member, reason: str, temporary: bool = False, end_time: float = None) -> None:
-        role = discord.utils.get(ctx.guild.roles, id=config.role_muted)
+        role = discord.utils.get(ctx.guild.roles, id=settings.get_value("role_muted"))
         await member.add_roles(role, reason=reason)
 
         # Open a connection to the database.
@@ -62,7 +62,7 @@ class MuteCog(Cog):
         moderator = ctx.author if ctx else self.bot.user
 
         # Removes "Muted" role from member.
-        role = discord.utils.get(guild.roles, id=config.role_muted)
+        role = discord.utils.get(guild.roles, id=settings.get_value("role_muted"))
         await member.remove_roles(role, reason=reason)
 
         # Open a connection to the database.
@@ -82,7 +82,7 @@ class MuteCog(Cog):
 
     @staticmethod
     async def is_user_muted(ctx: SlashContext, member: discord.Member) -> bool:
-        if discord.utils.get(ctx.guild.roles, id=config.role_muted) in member.roles:
+        if discord.utils.get(ctx.guild.roles, id=settings.get_value("role_muted")) in member.roles:
             return True
         return False
 
@@ -137,13 +137,13 @@ class MuteCog(Cog):
         if not duration:
             duration = "Indefinite"
 
-        # Create a channel in the category specified in the config.     
-        category = discord.utils.get(ctx.guild.categories, id=config.ticket_category_id)
+        # Create a channel in the category specified in settings.
+        category = discord.utils.get(ctx.guild.categories, id=settings.get_value("category_tickets"))
         channel = await ctx.guild.create_text_channel(f"mute-{member.id}", category=category)
 
         # Give both the staff and the user perms to access the channel. 
-        await channel.set_permissions(discord.utils.get(ctx.guild.roles, id=config.role_trial_mod), read_messages=True)
-        await channel.set_permissions(discord.utils.get(ctx.guild.roles, id=config.role_staff), read_messages=True)
+        await channel.set_permissions(discord.utils.get(ctx.guild.roles, id=settings.get_value("role_trial_mod")), read_messages=True)
+        await channel.set_permissions(discord.utils.get(ctx.guild.roles, id=settings.get_value("role_staff")), read_messages=True)
         await channel.set_permissions(member, read_messages=True)
 
         # Create embed at the start of the channel letting the user know how long they're muted for and why.
@@ -171,7 +171,7 @@ class MuteCog(Cog):
             return
 
         guild = guild or ctx.guild
-        category = discord.utils.get(guild.categories, id=config.ticket_category_id)
+        category = discord.utils.get(guild.categories, id=settings.get_value("category_tickets"))
         mute_channel = discord.utils.get(category.channels, name=f"mute-{user_id}")
 
         # Open a connection to the database.
@@ -209,8 +209,8 @@ class MuteCog(Cog):
         mod_list.add(muter)
 
         # Fetch the staff and trial mod role.
-        role_staff = discord.utils.get(guild.roles, id=config.role_staff)
-        role_trial_mod = discord.utils.get(guild.roles, id=config.role_trial_mod)
+        role_staff = discord.utils.get(guild.roles, id=settings.get_value("role_staff"))
+        role_trial_mod = discord.utils.get(guild.roles, id=settings.get_value("role_trial_mod"))
 
         # TODO: Implement so it gets the channel when the moderator is the bot
         # Loop through all messages in the ticket from old to new.
@@ -274,7 +274,7 @@ class MuteCog(Cog):
         embed.add_field(name="Mute Log: ", value=url, inline=False)
 
         # Send the embed to #mute-log.
-        mute_log = discord.utils.get(guild.channels, id=config.mute_log)
+        mute_log = discord.utils.get(guild.channels, id=settings.get_value("channel_mute_log"))
         await mute_log.send(embed=embed)
 
         # Delete the mute channel.
@@ -285,7 +285,7 @@ class MuteCog(Cog):
     @cog_ext.cog_slash(
         name="mute",
         description="Mutes a member in the server",
-        guild_ids=[config.guild_id],
+        guild_ids=[settings.get_value("guild_id")],
         options=[
             create_option(
                 name="member",
@@ -308,9 +308,9 @@ class MuteCog(Cog):
         ],
         default_permission=False,
         permissions={
-            config.guild_id: [
-                create_permission(config.role_staff, SlashCommandPermissionType.ROLE, True),
-                create_permission(config.role_trial_mod, SlashCommandPermissionType.ROLE, True)
+            settings.get_value("guild_id"): [
+                create_permission(settings.get_value("role_staff"), SlashCommandPermissionType.ROLE, True),
+                create_permission(settings.get_value("role_trial_mod"), SlashCommandPermissionType.ROLE, True)
             ]
         }
     )
@@ -373,9 +373,9 @@ class MuteCog(Cog):
 
         # Start creating the embed that will be used to alert the moderator that the user was successfully muted.
         embed = embeds.make_embed(
-            ctx=ctx, 
-            title=f"Muting member: {member}", 
-            thumbnail_url="https://i.imgur.com/rHtYWIt.png", 
+            ctx=ctx,
+            title=f"Muting member: {member}",
+            thumbnail_url="https://i.imgur.com/rHtYWIt.png",
             color="soft_red"
         )
         embed.description = f"{member.mention} was muted by {ctx.author.mention} for: {reason}"
@@ -397,7 +397,7 @@ class MuteCog(Cog):
     @cog_ext.cog_slash(
         name="unmute",
         description="Unmutes a member in the server",
-        guild_ids=[config.guild_id],
+        guild_ids=[settings.get_value("guild_id")],
         options=[
             create_option(
                 name="member",
@@ -414,9 +414,9 @@ class MuteCog(Cog):
         ],
         default_permission=False,
         permissions={
-            config.guild_id: [
-                create_permission(config.role_staff, SlashCommandPermissionType.ROLE, True),
-                create_permission(config.role_trial_mod, SlashCommandPermissionType.ROLE, True)
+            settings.get_value("guild_id"): [
+                create_permission(settings.get_value("role_staff"), SlashCommandPermissionType.ROLE, True),
+                create_permission(settings.get_value("role_trial_mod"), SlashCommandPermissionType.ROLE, True)
             ]
         }
     )
@@ -449,9 +449,9 @@ class MuteCog(Cog):
 
         # Start creating the embed that will be used to alert the moderator that the user was successfully unmuted.
         embed = embeds.make_embed(
-            ctx=ctx, 
-            title=f"Unmuting member: {member.name}", 
-            color="soft_green", 
+            ctx=ctx,
+            title=f"Unmuting member: {member.name}",
+            color="soft_green",
             thumbnail_url="https://i.imgur.com/W7DpUHC.png"
         )
         embed.description = f"{member.mention} was unmuted by {ctx.author.mention} for: {reason}"
