4a2ea7d3a021cd10541437733e0359d426290bc7
[REDACTED] <vikee.email@gmail.com>
2021-09-05 15:19:06 +0530

    Write embed generator.

--- cogs/commands/moderation/administration.py
+++ cogs/commands/moderation/administration.py
@@ -9,6 +9,9 @@ from contextlib import redirect_stdout
 import discord
 from discord.ext import commands
 from discord.ext.commands import Cog, Bot, Context
+from discord_slash import cog_ext, SlashContext
+from discord_slash.model import SlashCommandPermissionType
+from discord_slash.utils.manage_commands import create_choice, create_option, create_permission
 
 from cogs.commands import settings
 from utils import embeds
@@ -178,27 +181,44 @@ class AdministrationCog(Cog):
         await ctx.send(embed=embed)
 
         # The actual rules embed
-        embed = embeds.make_embed(title="📃  Discord Server Rules", color="quotes_grey", description="This list is not all-encompassing and you may be actioned for a reason outside of these rules. Use common sense when interacting in our community.")
-        embed.add_field(name="Rule 1: Do not send copyright-infringing material.", inline=False, value="> Linking to torrents, pirated stream links, direct download links, or uploading files over Discord puts our community at risk of being shut down. We are a discussion community, not a file-sharing hub.")
-        embed.add_field(name="Rule 2: Be courteous and mindful of others.", inline=False, value="> Do not engage in toxic behavior such as spamming, derailing conversations, attacking other users, or attempting to instigate drama. Bigotry will not be tolerated. Avoid problematic avatars, usernames, or nicknames.")
-        embed.add_field(name="Rule 3: Do not post self-promotional content.", inline=False, value="> We are not a billboard nor the place to advertise your Discord server, app, website, service, etc.")
-        embed.add_field(name="Rule 4: Do not post unmarked spoilers.", inline=False, value="> Use spoiler tags and include what series or episode your spoiler is in reference to outside the spoiler tag so people don't blindly click a spoiler.")
-        embed.add_field(name="Rule 5: Do not backseat moderate.", inline=False, value="> If you see someone breaking the rules or have something to report, please submit a <#829861810999132160> ticket.")
-        embed.add_field(name="Rule 6: Do not abuse pings.", inline=False, value="> Do not ping staff outside of conversation unless necessary. Do not ping VIP users for questions or help with their service. Do not spam or ghost ping other users.")
-        embed.add_field(name="Rule 7: Do not beg, buy, sell, or trade.", inline=False, value="> This includes, but is not limited to, server ranks, roles, permissions, giveaways, private community invites, or any digital or physical goods.")
-        embed.add_field(name="Rule 8: Follow the Discord Community Guidelines and Terms of Service.", inline=False, value="> The Discord Community Guidelines and Terms of Service govern all servers on the platform. Please familarize yourself with them and the restrictions that come with them. \n> \n> https://discord.com/guidelines \n> https://discord.com/terms")
+        embed = embeds.make_embed(title="📃  Discord Server Rules", color="quotes_grey",
+                                  description="This list is not all-encompassing and you may be actioned for a reason outside of these rules. Use common sense when interacting in our community.")
+        embed.add_field(name="Rule 1: Do not send copyright-infringing material.", inline=False,
+                        value="> Linking to torrents, pirated stream links, direct download links, or uploading files over Discord puts our community at risk of being shut down. We are a discussion community, not a file-sharing hub.")
+        embed.add_field(name="Rule 2: Be courteous and mindful of others.", inline=False,
+                        value="> Do not engage in toxic behavior such as spamming, derailing conversations, attacking other users, or attempting to instigate drama. Bigotry will not be tolerated. Avoid problematic avatars, usernames, or nicknames.")
+        embed.add_field(name="Rule 3: Do not post self-promotional content.", inline=False,
+                        value="> We are not a billboard nor the place to advertise your Discord server, app, website, service, etc.")
+        embed.add_field(name="Rule 4: Do not post unmarked spoilers.", inline=False,
+                        value="> Use spoiler tags and include what series or episode your spoiler is in reference to outside the spoiler tag so people don't blindly click a spoiler.")
+        embed.add_field(name="Rule 5: Do not backseat moderate.", inline=False,
+                        value="> If you see someone breaking the rules or have something to report, please submit a <#829861810999132160> ticket.")
+        embed.add_field(name="Rule 6: Do not abuse pings.", inline=False,
+                        value="> Do not ping staff outside of conversation unless necessary. Do not ping VIP users for questions or help with their service. Do not spam or ghost ping other users.")
+        embed.add_field(name="Rule 7: Do not beg, buy, sell, or trade.", inline=False,
+                        value="> This includes, but is not limited to, server ranks, roles, permissions, giveaways, private community invites, or any digital or physical goods.")
+        embed.add_field(name="Rule 8: Follow the Discord Community Guidelines and Terms of Service.", inline=False,
+                        value="> The Discord Community Guidelines and Terms of Service govern all servers on the platform. Please familarize yourself with them and the restrictions that come with them. \n> \n> https://discord.com/guidelines \n> https://discord.com/terms")
         await ctx.send(embed=embed)
 
         # /r/animepiracy links embed
         embed = embeds.make_embed(title="🔗  Our Links", color="quotes_grey")
-        embed.add_field(name="Reddit:", inline=True, value="> [/r/animepiracy](https://reddit.com/r/animepiracy)")
-        embed.add_field(name="Discord:", inline=True, value="> [discord.gg/piracy](https://discord.gg/piracy)")
-        embed.add_field(name="Index:", inline=True, value="> [piracy.moe](https://piracy.moe)")
-        embed.add_field(name="Wiki:", inline=True, value="> [wiki.piracy.moe](https://wiki.piracy.moe)")
-        embed.add_field(name="Seadex:", inline=True, value="> [releases.moe](https://releases.moe)")
-        embed.add_field(name="GitHub:", inline=True, value="> [github.com/ranimepiracy](https://github.com/ranimepiracy)")
-        embed.add_field(name="Twitter:", inline=True, value="> [@ranimepiracy](https://twitter.com/ranimepiracy)")
-        embed.add_field(name="Uptime Status:", inline=True, value="> [status.piracy.moe](https://status.piracy.moe/)")
+        embed.add_field(name="Reddit:", inline=True,
+                        value="> [/r/animepiracy](https://reddit.com/r/animepiracy)")
+        embed.add_field(name="Discord:", inline=True,
+                        value="> [discord.gg/piracy](https://discord.gg/piracy)")
+        embed.add_field(name="Index:", inline=True,
+                        value="> [piracy.moe](https://piracy.moe)")
+        embed.add_field(name="Wiki:", inline=True,
+                        value="> [wiki.piracy.moe](https://wiki.piracy.moe)")
+        embed.add_field(name="Seadex:", inline=True,
+                        value="> [releases.moe](https://releases.moe)")
+        embed.add_field(name="GitHub:", inline=True,
+                        value="> [github.com/ranimepiracy](https://github.com/ranimepiracy)")
+        embed.add_field(name="Twitter:", inline=True,
+                        value="> [@ranimepiracy](https://twitter.com/ranimepiracy)")
+        embed.add_field(name="Uptime Status:", inline=True,
+                        value="> [status.piracy.moe](https://status.piracy.moe/)")
         await ctx.send(embed=embed)
 
         # Clean up the command invoker
@@ -212,7 +232,8 @@ class AdministrationCog(Cog):
         embed = embeds.make_embed(title="🎫 Create a new modmail ticket",
                                   description="Click the react below to create a new modmail ticket.",
                                   color="default")
-        embed.add_field(name="Warning:", value="Serious inquiries only. Abuse may result in warning or ban.")
+        embed.add_field(
+            name="Warning:", value="Serious inquiries only. Abuse may result in warning or ban.")
         spawned = await ctx.send(embed=embed)
         await spawned.add_reaction("🎫")
         await ctx.message.delete()
@@ -222,7 +243,8 @@ class AdministrationCog(Cog):
     @commands.before_invoke(record_usage)
     @commands.command(name="createcolorrolesembed", aliases=['ccre'])
     async def create_color_roles_embed(self, ctx: Context):
-        embed = discord.Embed(description=f"You can react to one of the squares below to be assigned a colored user role. If you are interested in a different color, you can become a <@&{settings.get_value('role_server_booster')}> to receive a custom colored role.")
+        embed = discord.Embed(
+            description=f"You can react to one of the squares below to be assigned a colored user role. If you are interested in a different color, you can become a <@&{settings.get_value('role_server_booster')}> to receive a custom colored role.")
         msg = await ctx.send(embed=embed)
 
         # API call to fetch all the emojis to cache, so that they work in future calls
@@ -267,6 +289,97 @@ class AdministrationCog(Cog):
         await msg.add_reaction("🧩")
         await msg.add_reaction(":pickaxe:831765423455993888")
         await ctx.message.delete()
+
+    @commands.bot_has_permissions(embed_links=True, send_messages=True)
+    @commands.before_invoke(record_usage)
+    @cog_ext.cog_slash(
+        name="create_embed",
+        description="Creates an embed from the specified parameters.",
+        guild_ids=[settings.get_value("guild_id")],
+        options=[
+            create_option(
+                name="title",
+                description="Embed title.",
+                option_type=3,
+                required=False
+            ),
+            create_option(
+                name="description",
+                description="Embed description.",
+                option_type=3,
+                required=False
+            ),
+            create_option(
+                name="url",
+                description="Embed URL",
+                option_type=3,
+                required=False
+            ),
+            create_option(
+                name="footer",
+                description="Embed footer.",
+                option_type=3,
+                required=False
+            ),
+            create_option(
+                name="footer_icon_url",
+                description="Embed footer icon URL.",
+                option_type=3,
+                required=False
+            ),
+            create_option(
+                name="color",
+                description="Embed color.",
+                option_type=3,
+                required=False
+            ),
+            create_option(
+                name="use_author",
+                description="Whether to use the command invocation author as embed author.",
+                option_type=5,
+                required=False
+            ),
+            create_option(
+                name="thumbnail_url",
+                description="URL for the thumbnail parameter of the embed.",
+                option_type=3,
+                required=False
+            ),
+            create_option(
+                name="image_url",
+                description="URL for the image parameter of the embed.",
+                option_type=3,
+                required=False
+            )
+        ],
+        default_permission=False,
+        permissions={
+            settings.get_value("guild_id"): [
+                create_permission(settings.get_value(
+                    "role_staff"), SlashCommandPermissionType.ROLE, True),
+                create_permission(settings.get_value(
+                    "role_trial_mod"), SlashCommandPermissionType.ROLE, True)
+            ]
+        }
+    )
+    async def embed_generator(self, ctx: SlashContext, title: str = "", description: str = "", url: str = "", footer: str = "", footer_icon_url: str = "",  color: str = "default", use_author: bool = True, thumbnail_url: str = None, image_url: str = None):
+        await ctx.defer()
+        
+        embed = embeds.make_embed(ctx=ctx, title=title, description=description, color=color,
+                                  thumbnail_url=thumbnail_url, image_url=image_url, author=use_author)
+        
+        if not use_author:
+            embed.timestamp = discord.Embed.Empty
+        
+        if footer:
+            embed.set_footer(text=footer, icon_url=footer_icon_url)
+
+        embed.url = url
+
+        if len(embed) > 6000:
+            await embeds.error_message(ctx=ctx, description="The embed is too big!")
+    
+        await ctx.send(embed=embed)
 
 
 def setup(bot: Bot) -> None:
