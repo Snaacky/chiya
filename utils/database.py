b74741dd38de24115ca81bf7f9449e34ce7172c4
Snaacky <snaacky@pm.me>
2021-10-01 12:34:19 -0400

    Migrated database credentials back to YAML

--- utils/database.py
+++ utils/database.py
@@ -5,23 +5,24 @@ import dataset
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 
+from utils.config import config
+
 log = logging.getLogger(__name__)
 
 
 class Database:
     def __init__(self) -> None:
-        self.type = os.getenv("TYPE").lower()
-        self.host = os.getenv("HOST")
-        self.database = os.getenv("DATABASE")
-        self.user = os.getenv("USER")
-        self.password = os.getenv("PASSWORD")
+        self.type = config["database"]["type"].lower()
+        self.host = config["database"]["host"]
+        self.database = config["database"]["database"]
+        self.user = config["database"]["user"]
+        self.password = config["database"]["password"]
 
         if self.type == "mysql" and not all([self.type, self.database, self.host, self.user, self.password]):
             log.error("One or more MySQL connection variables are missing, exiting...")
             raise SystemExit
         elif self.type == "sqlite" and not all([self.type, self.database]):
             log.error("One or more SQLite connection variables are missing, exiting...")
-            raise SystemExit
 
         if self.type == "mysql":
             self.url = f"mysql://{self.user}:{self.password}@{self.host}/{self.database}"
