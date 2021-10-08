ee206686fdaba134ba3b0000a824650a08bb686c
Snaacky <snaacky@pm.me>
2021-10-08 15:48:40 -0400

    Removed docstrings, added Typing, fixed spacing, removed unused imports
    - Removed the unnecessary `import Discord` and changed all of the `: Member` typing to `: discord.Member`
    - Added typing for functions that were previously missing it.
    - Removed the verbose boilerplate docstrings because the typing already explains what is going on.
    - Fixed trailing whitespace on some lines.

--- cogs/listeners/boosts_handle.py
+++ cogs/listeners/boosts_handle.py
@@ -1,7 +1,6 @@
 import logging
 
 import discord
-from discord import Member
 from discord.ext import commands
 
 from utils import embeds
@@ -15,52 +14,17 @@ class BoostsHandler(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-
     @commands.Cog.listener()
-    async def on_member_update(self, before: Member, after: Member) -> None:
-        """
-        Event Listener which is called when a Member updates their profile.
-
-        Args:
-            before (Member): The updated member’s old info.
-            after (Member): The updated member’s updated info.
-
-        Note:
-            This requires Intents.members to be enabled.
-
-        For more information:
-            https://discordpy.readthedocs.io/en/latest/api.html#discord.on_member_update
-        """ 
+    async def on_member_update(self, before: discord.Member, after: discord.Member) -> None:
         await self.process_new_booster(before, after)
         await self.process_lost_booster(before, after)
 
-
     @commands.Cog.listener()
     async def on_guild_update(self, before: discord.Guild, after: discord.Guild) -> None:
-        """Event Listener which is called when a Guild updates.
-
-        Args:
-            before (discord.Guild): The guild prior to being updated.
-            after (discord.Guild): The guild after being updated.
-
-        Note:
-            This requires Intents.guilds to be enabled.
-
-        For more information:
-            https://discordpy.readthedocs.io/en/stable/api.html#discord.on_guild_update
-        """
         await self.on_new_boost(before, after)
         await self.on_removed_boost(before, after)
 
-
-    async def on_new_boost(self, before, after):
-        """ 
-        Process whenever a new boost is received.
-
-        Parameters:
-            before (discord.Guild): The guild prior to being updated.
-            after  (discord.Guild): The guild after being updated.
-        """
+    async def on_new_boost(self, before: discord.Guild, after: discord.Guild):
         # TODO: Replace hardcoded mention with a .mention from config.
         if after.premium_subscription_count > before.premium_subscription_count:
             embed = embeds.make_embed(
@@ -72,7 +36,7 @@ class BoostsHandler(commands.Cog):
                     "[hex color](https://www.google.com/search?q=hex+color) "
                     "and your desired role name for a custom booster role."
                 ),
-                author=False, 
+                author=False,
                 color="nitro_pink",
                 image_url="https://i.imgur.com/O8R98p9.gif"
             )
@@ -81,45 +45,29 @@ class BoostsHandler(commands.Cog):
             # Attempts to create a link *near* where the boost occurred.
             nitro_logs = discord.utils.get(after.channels, id=config["channels"]["nitro_log"])
             embed = embeds.make_embed(
-                description = (
+                description=(
                     "[A new boost was added to the server.]",
                     "(https://canary.discord.com/channels/",
                     f"{after.id}/{after.system_channel.id}/{after.system_channel.last_message_id})"
                 ),
-                author=False, 
+                author=False,
                 color="nitro_pink",
             )
             await nitro_logs.send(embed=embed)
             log.info(f"A new boost was added to {after.name}.")
 
-
-    async def on_removed_boost(self, before, after):
-        """ 
-        Process whenever a new boost is removed.
-
-        Parameters:
-            before (discord.Guild): The guild prior to being updated.
-            after  (discord.Guild): The guild after being updated.
-        """
+    async def on_removed_boost(self, before: discord.Guild, after: discord.Guild):
         if after.premium_subscription_count < before.premium_subscription_count:
             nitro_logs = discord.utils.get(after.channels, id=config["channels"]["nitro_log"])
             embed = embeds.make_embed(
-                description=f"A boost was removed from the server.",
-                author=False, 
+                description="A boost was removed from the server.",
+                author=False,
                 color="nitro_pink"
             )
             await nitro_logs.send(embed=embed)
             log.info(f"A boost was removed from {after.name}.")
 
-
-    async def process_new_booster(self, before, after):
-        """ 
-        Process when a user who previously hasn't boosted the server boosts.
-
-        Parameters:
-            before (discord.Member): The updated member’s old info.
-            after  (discord.Member): The updated member’s updated info.
-        """
+    async def process_new_booster(self, before: discord.Member, after: discord.Member):
         if not before.premium_since and after.premium_since:
             channel = discord.utils.get(after.guild.channels, id=config["channels"]["nitro_log"])
             embed = embeds.make_embed(
@@ -134,25 +82,17 @@ class BoostsHandler(commands.Cog):
             await channel.send(embed=embed)
             log.info(f'{after} boosted {after.guild.name}.')
 
-
-    async def process_lost_booster(self, before, after):
-        """ 
-        Process when a user who previously boosted the server removes all of their boosts from the server.
-
-        Parameters:
-            before (discord.Member): The updated member’s old info.
-            after  (discord.Member): The updated member’s updated info.
-        """
+    async def process_lost_booster(self, before: discord.Member, after: discord.Member):
         # Send an embed in #nitro-logs that someone stopped boosting the server.
         if before.premium_since and not after.premium_since:
             channel = discord.utils.get(after.guild.channels, id=config["channels"]["nitro_log"])
             embed = embeds.make_embed(
-                title = "Lost booster",
+                title="Lost booster",
                 description=(
                     f"{after.mention} no longer boosts the server. ",
                     f"We're now at {after.guild.premium_subscription_count} boosts."
                 ),
-                author=False, 
+                author=False,
                 color="nitro_pink"
                 )
             await channel.send(embed=embed)
