bcc4fafc84131ece07f02e0b2ae50913e5600cbd
Mint <developer@chocomint.dev>
2021-12-13 18:22:33 -0500

    Ported move_questions to pycord

--- cogs/apps/move_question.py
+++ cogs/apps/move_question.py
@@ -1,39 +1,33 @@
 import logging
 
 import discord
-from discord.ext.commands import Cog, Bot
-from discord_slash import cog_ext
-from discord_slash.context import MenuContext
-from discord_slash.model import ContextMenuType
+from discord import message_command, ApplicationContext
+from discord.commands import context
+from discord.ext import commands
 
-from utils.config import config
 from utils import embeds
-
+from utils.config import config
 
 log = logging.getLogger(__name__)
 
 
-class MoveQuestionCog(Cog):
-
+class MoveQuestionApps(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @cog_ext.cog_context_menu(
-        target=ContextMenuType.MESSAGE,
-        name="Move question",
-        guild_ids=config["guild_ids"]
-    )
-    async def move_question(self, ctx: MenuContext):
+    @message_command(guild_ids=config["guild_ids"], name="test")
+    async def move_question(self, ctx: context.ApplicationContext, message: discord.Message):
         """
         Context menu command for moving questions (messages) to #questions-and-help.
 
         Args:
-            ctx (MenuContext): The context of the context menu.
+            ctx (ApplicationContext): The context of the context menu.
+            message (discord.Message):
 
         TODO:
             Fix "Deferred response might not be what you set it to!" warning.
         """
-        await ctx.defer(hidden=True)
+        await ctx.defer()
 
         staff = [x for x in ctx.author.roles
                  if x.id == config["roles"]["staff"]
@@ -51,32 +45,33 @@ class MoveQuestionCog(Cog):
                 description="You do not have permissions to use this command in this category."
             )
 
-        channel = discord.utils.get(ctx.guild.text_channels, id=config["channels"]["questions_and_help"])
-        webhook = await channel.create_webhook(name=ctx.target_message.author)
+        channel = discord.utils.get(ctx.guild.text_channels, id=config["channels"]["mod"]["bot_testing"])
+        webhook = await channel.create_webhook(name=ctx.author.name)
 
         await webhook.send(
-            str(ctx.target_message.clean_content),
-            username=ctx.target_message.author.name,
-            avatar_url=ctx.target_message.authoravatar
+            content=message.content,
+            username=ctx.author.name,
+            avatar_url=ctx.author.avatar
         )
+
         await webhook.delete()
-        await ctx.target_message.delete()
+        await ctx.delete()
 
         await embeds.success_message(ctx=ctx, description=f"Successfully moved message to: {channel.mention}")
         await embeds.warning_message(
             ctx=ctx,
             title="Warning: Your question was moved",
             description=(
-                f"{ctx.target_message.author.mention}, your message was moved to {channel.mention} "
+                f"{ctx.author.mention}, your message was moved to {channel.mention} "
                 "which is the more appropriate channel for help, questions, and support type "
                 "topics. Please continue your conversation in that channel."
             ),
             author=False
         )
-        ping = await channel.send(ctx.target_message.author.mention)
+        ping = await channel.send(ctx.author.mention)
         await ping.delete()
 
 
-def setup(bot: Bot) -> None:
-    bot.add_cog(MoveQuestionCog(bot))
+def setup(bot: commands.bot.Bot) -> None:
+    bot.add_cog(MoveQuestionApps(bot))
     log.info("App loaded: move_question")
