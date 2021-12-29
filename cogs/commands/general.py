22daa14a234b0c461981e19766e253abc3069b64
Snaacky <snaacky@pm.me>
2021-12-29 08:56:07 -0500

    Adjusted docstring format. Added ability to get server avatar with /pfp.

--- cogs/commands/general.py
+++ cogs/commands/general.py
@@ -17,40 +17,37 @@ class GeneralCommands(commands.Cog):
         self.bot = bot
 
     @slash_command(guild_ids=config["guild_ids"], description="Gets a users profile picture")
-    async def pfp(self, ctx: context.ApplicationContext, user: Option(discord.User, required=False)) -> bool:
+    async def pfp(
+        self,
+        ctx: context.ApplicationContext,
+        user: Option(discord.User, description="User whose avatar will be grabbed", required=False),
+        guild_avatar: Option(bool, description="Do you want their server specific avatar", required=False)
+    ) -> bool:
         """
-        Gets a users Discord profile picture (global, not server-specific).
+        Grab a user's avatar and return it in a large-sized embed.
 
-        If the user argument is specified, it will return that users profile picture.
-        Otherwise, it will return the invokers profile picture.
-
-        Args:
-            ctx (context.ApplicationContext): The context for the function invoke.
-            user (discord.User, optional): The user to get the profile picture for.
-
-        Returns:
-            True (bool): If the profile picture was successfully grabbed.
+        If the user parameter is not specified, the function will grab the invokers avatar instead.
+        Offers the ability to attempt to grab a users server avatar and will fallback to their global
+        avatar with a warning attached if a server specific avatar is not set.
         """
-        # TODO: add an optional boolean to get the server specific pfp
-        # TODO: what happens if a user ID is specified and that user ID is invalid?
         await ctx.defer()
 
         user = user or ctx.author
-
         if isinstance(user, int):
             user = await self.bot.fetch_user(user)
 
-        # TODO: this if-statement logic can probably be optimized
-        if ctx.author:
-            embed = embeds.make_embed(ctx=ctx)
-
-        if user:
-            embed = embeds.make_embed()
+        embed = embeds.make_embed()
+        if guild_avatar and hasattr(user, "guild_avatar"):
+            embed.set_author(icon_url=user.guild_avatar, name=str(user))
+            embed.set_image(url=user.guild_avatar)
+        elif guild_avatar and not hasattr(user, "guild_avatar"):
             embed.set_author(icon_url=user.avatar.url, name=str(user))
-
-        embed.set_image(url=user.avatar.url)
+            embed.set_image(url=user.avatar.url)
+            embed.set_footer(text="⚠️ Warning: Could not find server avatar, defaulted to global avatar.")
+        else:
+            embed.set_author(icon_url=user.avatar.url, name=str(user))
+            embed.set_image(url=user.avatar.url)
         await ctx.send_followup(embed=embed)
-        return True
 
     @slash_command(guild_ids=config["guild_ids"], default_permission=False, description="Add vote reactions to a message.")
     @permissions.has_role(config["roles"]["staff"])
@@ -64,14 +61,6 @@ class GeneralCommands(commands.Cog):
 
         If the message argument is specified, it will add the reactions to that message.
         Otherwise, it will add the reactions to the last message in the channel.
-
-        Args:
-            ctx (context.ApplicationContext): Context for the function invoke.
-            message (str, optional): Message ID to add the vote reactions to.
-
-        Returns:
-            True (bool): Vote reactions were successfully added.
-            False (bool): Message ID provider does not exist, unsuccessful.
         """
         # TODO: what happens if the user doesn't have permission to add reactions in that channel?
         await ctx.defer()
