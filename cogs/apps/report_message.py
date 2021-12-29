6eaa1d923630c18ff2247519897ca12869b1fbff
Mint <developer@chocomint.dev>
2021-12-29 15:57:15 -0500

    Added close button, improved code logic, improved interaction

--- cogs/apps/report_message.py
+++ cogs/apps/report_message.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 import discord
 from discord import message_command
@@ -11,6 +12,35 @@ from utils.config import config
 log = logging.getLogger(__name__)
 
 
+@commands.Cog.listener()
+async def on_ready(self):
+    """ Register the close report button that persists between bot restarts. """
+    self.bot.add_view(ReportCloseButton())
+
+
+class ReportCloseButton(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    @discord.ui.button(label="Close Report", style=discord.ButtonStyle.danger, custom_id="close_report", emoji="🔒")
+    async def close(self, button: discord.ui.Button, interaction: discord.Interaction):
+        """ The close button to close and archive an existing report. """
+        role_staff = discord.utils.get(interaction.guild.roles, id=config["roles"]["staff"])
+        if role_staff not in interaction.user.roles:
+            embed = embeds.make_embed(
+                title="Failed to close report",
+                description="You do not have the permission to close this report.",
+                color=discord.Color.red()
+            )
+            return await interaction.response.send_message(embed=embed, ephemeral=True)
+
+        close_embed = embeds.make_embed(color=discord.Color.blurple(), description="The report will be closed shortly...")
+        await interaction.response.send_message(embed=close_embed)
+
+        time.sleep(3)
+        await interaction.channel.delete()
+
+
 class ReportMessageButtons(discord.ui.View):
     def __init__(self):
         super().__init__()
@@ -19,6 +49,7 @@ class ReportMessageButtons(discord.ui.View):
     @discord.ui.button(label="Submit Report", style=discord.ButtonStyle.primary, custom_id="submit_report")
     async def submit(self, button: discord.ui.Button, interaction: discord.Interaction):
         """ Create a View for the report message embed confirmation button. """
+        await interaction.response.edit_message(view=None)
         self.value = True
         self.stop()
 
@@ -27,6 +58,7 @@ class ReportMessageButtons(discord.ui.View):
         """ Create a View for the report message embed cancel button. """
         embed = embeds.make_embed(description="Your report has been canceled.")
         await interaction.response.edit_message(embed=embed, view=None)
+        self.stop()
 
 
 class ReportMessageApp(commands.Cog):
@@ -61,18 +93,19 @@ class ReportMessageApp(commands.Cog):
                 "Are you sure you want to report this message? "
                 "Reporting this message will open a new report ticket and staff will be alerted to your report."
             ),
-            footer="Abusing reports will result in a ban. Only use this feature for serious reports.",
             fields=[
                 {"name": "Author:", "value": message.author.mention, "inline": True},
                 {"name": "Channel:", "value": message.channel.mention, "inline": True},
-            ])
+                {"name": "Link:", "value": message.jump_url, "inline": False},
+            ],
+            footer="Abusing reports will result in a ban. Only use this feature for serious reports.",
+            color=discord.Color.blurple(),
+        )
 
         if message.clean_content:
             embed.add_field(name="Message:", value=f">>> {message.clean_content[0:1023]}", inline=False)
         for attachment in message.attachments:
             embed.add_field(name="Attachment:", value=attachment.url, inline=False)
-
-        embed.add_field(name="Link:", value=message.jump_url, inline=False)
 
         view = ReportMessageButtons()
         await ctx.send_followup(embed=embed, view=view, ephemeral=True)
@@ -90,7 +123,13 @@ class ReportMessageApp(commands.Cog):
 
             embed = embeds.make_embed(
                 title="Reported message",
+                description=(
+                    "Staff have been alerted about your report. "
+                    "Your report will be reviewed at their earliest convenience. "
+                    "If you have any supporting details you would like to add to your report, you may do so now."
+                ),
                 footer="Reported message originally sent",
+                timestamp=message.created_at,
                 fields=[
                     {"name": "Author:", "value": message.author.mention, "inline": True},
                     {"name": "Channel:", "value": message.channel.mention, "inline": True},
@@ -104,25 +143,17 @@ class ReportMessageApp(commands.Cog):
             for attachment in message.attachments:
                 embed.add_field(name="Attachment:", value=attachment.url, inline=False)
 
-            embed.timestamp = message.created_at
-            await channel.send(embed=embed)
+            await channel.send(embed=embed, view=ReportCloseButton())
 
-            embed = embeds.make_embed(
-                description=(
-                    "Staff have been alerted about your report. "
-                    "Your report will be reviewed at their earliest convenience. "
-                    "If you have any supporting details you would like to add to your report, you may do so now."
-                ))
-            await channel.send(embed=embed)
             await channel.send(ctx.author.mention, delete_after=1)
-            # await channel.send("@here", delete_after=1)
+            await channel.send("@here", delete_after=1)
 
-            embed = embeds.make_embed(
-                description=f"Your report has been successfully created: {channel.mention}",
-                color=discord.Color.green(),
+            success_embed = embeds.make_embed(
+                description=f"Your report has been submitted.",
+                color=discord.Color.blurple(),
             )
 
-            await ctx.send_followup(embed=embed, ephemeral=True)
+            await ctx.send_followup(embed=success_embed, ephemeral=True)
 
 
 def setup(bot: commands.bot.Bot) -> None:
