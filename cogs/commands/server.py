0b36ab16767cce32d667258a474323f2fcebd274
Snaacky <snaacky@pm.me>
2022-01-01 11:11:35 -0500

    Removed /server banner & pingable which were unused features

--- cogs/commands/server.py
+++ cogs/commands/server.py
@@ -35,58 +35,6 @@ class ServerCommands(commands.Cog):
         await ctx.defer()
         await ctx.send_followup(ctx.guild.member_count)
 
-    @server.command(name="banner", description="Sets the banner to the image provided")
-    async def banner(
-        self,
-        ctx: context.ApplicationContext,
-        link: Option(str, description="The link to the image to be set", required=True),
-    ) -> None:
-        """
-        Update the banner of the guild. Return if the HTTP status code
-        received is not 200 OK (invalid url).
-        """
-        await ctx.defer()
-
-        r = requests.get(url=link)
-
-        if r.status_code != 200:
-            return await embeds.error_message(ctx=ctx, description="The link you entered was not accessible.")
-
-        try:
-            await ctx.guild.edit(banner=r.content)
-        except discord.InvalidArgument:
-            return await embeds.error_message(ctx=ctx, description="Unable to set banner, verify the link is correct.")
-
-        embed = embeds.make_embed(
-            title="Banner updated",
-            description=f"Banner [image]({link}) updated by {ctx.author.mention}",
-            color=discord.Color.green(),
-        )
-
-        await ctx.send_followup(embed=embed)
-
-    @server.command(name="pingable", description="Makes a role pingable for 10 seconds")
-    async def pingable(
-        self,
-        ctx: context.ApplicationContext,
-        role: Option(discord.Role, description="The role to make pingable", required=True),
-    ) -> None:
-        """
-        Make a role pingable for 10 seconds.
-        """
-        await ctx.defer()
-
-        if role.mentionable:
-            return await embeds.success_message(ctx=ctx, description="That role is already mentionable.")
-
-        try:
-            await role.edit(mentionable=True)
-            await embeds.success_message(ctx=ctx, description="You have 10 seconds to ping the role.")
-            await asyncio.sleep(10)
-            await role.edit(mentionable=False)
-        except discord.Forbidden:
-            await embeds.error_message(ctx=ctx, description="The bot does not have permission to edit this role.")
-
     @server.command(name="boosters", description="List all the server boosters")
     async def boosters(self, ctx: context.ApplicationContext) -> None:
         """ Send an embed with all current server boosters. """
