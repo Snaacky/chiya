7c0521311f3d9568d5c21f08ca2147a58c4a3367
Mint <developer@chocomint.dev>
2022-01-20 02:02:48 -0500

    Fixed broken config path

--- chiya/__init__.py
+++ chiya/__init__.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 import coloredlogs
 
-from config import config
+from chiya.config import config
 
 
 log_level = config["bot"]["log_level"]
--- chiya/config.py
+++ chiya/config.py
@@ -5,8 +5,11 @@ from pyaml_env import parse_config
 
 
 log = logging.getLogger(__name__)
-if not os.path.isfile(os.path.join(os.getcwd(), "config.yml")):
+
+
+path = os.path.dirname(os.path.dirname(__file__))
+if not os.path.isfile(os.path.join(path, "config.yml")):
     log.error("Unable to load config.yml, exiting...")
     raise SystemExit
 
-config = parse_config(os.path.join(os.getcwd(), "config.yml"))
+config = parse_config(os.path.join(path, "config.yml"))
