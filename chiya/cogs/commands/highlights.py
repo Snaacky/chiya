71b82ce28640bf74bf3aca2f1e317049d56f809c
Snaacky <snaacky@pm.me>
2022-03-16 22:23:49 -0400

    Added /hl clear, made double quotes usage consistent, /hl list is broke

--- chiya/cogs/commands/highlights.py
+++ chiya/cogs/commands/highlights.py
@@ -1,16 +1,12 @@
-import asyncio
 import logging
 
 import discord
-from discord.commands import Option, SlashCommandGroup, context, slash_command
+from discord.commands import Option, SlashCommandGroup, context
 from discord.ext import commands
 import orjson
-from sqlalchemy import desc
 
 from chiya import config, database
 from chiya.utils import embeds
-from chiya.utils.highlights import refresh_cache
-from chiya.utils.pagination import LinePaginator
 
 
 log = logging.getLogger(__name__)
@@ -39,28 +35,27 @@ class HighlightCommands(commands.Cog):
         await ctx.defer()
 
         db = database.Database().get()
-        result = db['highlights'].find_one(term={"ilike": term})
+        result = db["highlights"].find_one(term={"ilike": term})
         if result:
             users = orjson.loads(result["users"])
             if ctx.author.id not in users:
                 users.append(ctx.author.id)
-                data = dict(id=result['id'], users=orjson.dumps(users))
-                db['highlights'].update(data, ["id"])
+                data = dict(id=result["id"], users=orjson.dumps(users))
+                db["highlights"].update(data, ["id"])
         else:
             data = dict(
                 term=term,
                 users=orjson.dumps([ctx.author.id])
             )
-            db['highlights'].insert(data)
+            db["highlights"].insert(data)
 
-        refresh_cache()
         db.commit()
         db.close()
 
         embed = embeds.make_embed(
             ctx=ctx,
-            title='Highlight added',
-            description=f'The term `{term}` was added to your highlights list.',
+            title="Highlight added",
+            description=f"The term `{term}` was added to your highlights list.",
             color=discord.Color.green(),
             author=True
         )
@@ -75,13 +70,13 @@ class HighlightCommands(commands.Cog):
         Renders a list showing all of the terms that the user currently has
         highlighted to be notified on usage of.
         """
-        # TODO: For some reason this sometimes doesn't output
+        # This has very inconsistent output and the if not results check does not seem to work consistently.
         await ctx.defer()
 
         db = database.Database().get()
-        results = db['highlights'].find(users={"ilike": f"%{ctx.author.id}%"})
+        results = db["highlights"].find(users={"ilike": f"%{ctx.author.id}%"})
 
-        if not any(results):
+        if not results:
             return await embeds.error_message(ctx=ctx, description="You are not tracking any terms.")
 
         embed = embeds.make_embed(
@@ -103,14 +98,14 @@ class HighlightCommands(commands.Cog):
         await ctx.defer()
 
         db = database.Database().get()
-        result = db['highlights'].find_one(term=term, users={"ilike": f"%{ctx.author.id}%"})
+        result = db["highlights"].find_one(term=term, users={"ilike": f"%{ctx.author.id}%"})
 
         if not result:
             return await embeds.error_message(ctx=ctx, description="You are not tracking that term.")
 
         users = orjson.loads(result["users"])
         users.remove(ctx.author.id)
-        db['highlights'].update(dict(id=result['id'], users=orjson.dumps(users)), ["id"])
+        db["highlights"].update(dict(id=result["id"], users=orjson.dumps(users)), ["id"])
 
         # Delete the term from the database if no users are tracking the keyword anymore.
         if not len(users):
@@ -118,12 +113,45 @@ class HighlightCommands(commands.Cog):
 
         db.commit()
         db.close()
-        refresh_cache()
 
         embed = embeds.make_embed(
             ctx=ctx,
-            title='Highlight removed',
-            description=f'The term `{term}` was removed from your highlights list.',
+            title="Highlight removed",
+            description=f"The term `{term}` was removed from your highlights list.",
+            color=discord.Color.green(),
+            author=True
+        )
+        await ctx.send_followup(embed=embed)
+
+    @highlight.command(name="clear", description="Clears all terms being tracked")
+    async def clear_highlights(
+        self,
+        ctx: context.ApplicationContext
+    ) -> None:
+        await ctx.defer()
+
+        db = database.Database().get()
+        results = db["highlights"].find(users={"ilike": f"%{ctx.author.id}%"})
+
+        if not results:
+            return await embeds.error_message(ctx=ctx, description="You are not tracking any terms.")
+
+        for result in results:
+            users = orjson.loads(result["users"])
+            users.remove(ctx.author.id)
+            db["highlights"].update(dict(id=result["id"], users=orjson.dumps(users)), ["id"])
+
+            # Delete the term from the database if no users are tracking the keyword anymore.
+            if not len(users):
+                db["highlights"].delete(term=result["term"])
+
+        db.commit()
+        db.close()
+
+        embed = embeds.make_embed(
+            ctx=ctx,
+            title="Highlights cleared",
+            description="All of the terms in your highlight list were cleared.",
             color=discord.Color.green(),
             author=True
         )
