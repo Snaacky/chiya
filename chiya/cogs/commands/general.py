52d09d81b205ee51991fa5e1c20b828ae31ef13f
REDACTED <vikee.email@gmail.com>
2022-04-13 16:06:06 +0530

    Initial commit

--- chiya/cogs/commands/general.py
+++ chiya/cogs/commands/general.py
@@ -15,12 +15,22 @@ class GeneralCommands(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    @slash_command(guild_ids=config["guild_ids"], description="Gets a users profile picture")
+    @slash_command(
+        guild_ids=config["guild_ids"], description="Gets a users profile picture"
+    )
     async def pfp(
         self,
         ctx: context.ApplicationContext,
-        user: Option(discord.User, description="User whose profile picture will be grabbed", required=False),
-        server: Option(bool, description="Prefer server profile picture (if one exists)", required=False),
+        user: Option(
+            discord.User,
+            description="User whose profile picture will be grabbed",
+            required=False,
+        ),
+        server: Option(
+            bool,
+            description="Prefer server profile picture (if one exists)",
+            required=False,
+        ),
     ) -> None:
         """
         Grab a user's avatar and return it in a large-sized embed.
@@ -43,20 +53,26 @@ class GeneralCommands(commands.Cog):
         elif server and user.guild_avatar is None:
             embed.set_author(icon_url=user.avatar.url, name=str(user))
             embed.set_image(url=user.avatar.url)
-            embed.set_footer(text="⚠️ Prefer server profile picture was specified but user does not have a server profile picture set.")
+            embed.set_footer(
+                text="⚠️ Prefer server profile picture was specified but user does not have a server profile picture set."
+            )
         else:
             embed.set_author(icon_url=user.avatar.url, name=str(user))
             embed.set_image(url=user.avatar.url)
         await ctx.send_followup(embed=embed)
 
     @slash_command(
-        guild_ids=config["guild_ids"], default_permission=False, description="Add vote reactions to a message."
+        guild_ids=config["guild_ids"],
+        default_permission=False,
+        description="Add vote reactions to a message.",
     )
     @permissions.has_role(config["roles"]["staff"])
     async def vote(
         self,
         ctx: context.ApplicationContext,
-        message: Option(str, description="The ID for the target message", required=False),
+        message: Option(
+            str, description="The ID for the target message", required=False
+        ),
     ) -> None:
         """
         Adds vote emojis (yes and no) reactions to a message.
@@ -72,18 +88,97 @@ class GeneralCommands(commands.Cog):
             try:
                 message = await ctx.channel.fetch_message(message)
             except discord.NotFound:
-                return await embeds.error_message(ctx=ctx, description="Invalid message ID.")
+                return await embeds.error_message(
+                    ctx=ctx, description="Invalid message ID."
+                )
 
         if not message:
             messages = await ctx.channel.history(limit=1).flatten()
             message = messages[0]
 
         # TODO: replace this with emotes grabbed from config
-        await message.add_reaction(":yes:778724405333196851")
-        await message.add_reaction(":no:778724416230129705")
-        await embeds.success_message(ctx=ctx, description=f"Added votes to {message.jump_url}")
+        await message.add_reaction(":yes:914162499023142996")
+        await message.add_reaction(":no:914162576403873832")
+        await embeds.success_message(
+            ctx=ctx, description=f"Added votes to {message.jump_url}"
+        )
+
+    @slash_command(
+        guild_ids=config["guild_ids"],
+        default_permission=False,
+        description="Summarises a vote, and displays results.",
+    )
+    @permissions.has_role(config["roles"]["staff"])
+    async def vote_info(
+        self,
+        ctx: context.ApplicationContext,
+        message: Option(
+            str, description="The ID for the target message", required=True
+        ),
+    ) -> None:
+        """
+        Summarises a vote, and displays results.
+        """
+        await ctx.defer()
+
+        if message:
+            try:
+                message = await ctx.channel.fetch_message(message)
+            except discord.NotFound:
+                return await embeds.error_message(
+                    ctx=ctx, description="Invalid message ID."
+                )
+
+        yes_reactions = None
+        no_reactions = None
+        
+        for reaction in message.reactions:
+            if reaction.emoji.id == 914162499023142996:
+                yes_reactions = reaction
+            if reaction.emoji.id == 914162576403873832:
+                no_reactions = reaction
+
+        if not yes_reactions or not no_reactions:
+            return await embeds.error_message(
+                ctx=ctx,
+                description="That message does not have the appropriate yes and no reactions.",
+            )
+
+        yes_users = []
+        async for user in yes_reactions.users():
+            if not user.bot:
+                yes_users.append(user.mention)
+
+        no_users = []
+        async for user in no_reactions.users():
+            if not user.bot:
+                no_users.append(user.mention)
+
+        both_users = set(yes_users).intersection(set(no_users))
+
+        staff = discord.utils.get(message.guild.roles, id=config['roles']['staff']).members
+        staff_users = []
+        for user in staff:
+            if not user.bot:
+                staff_users.append(user.mention)
+
+        skipped_users = set(staff_users).difference(
+            set(yes_users).union(set(no_users))
+        )
+
+        embed = embeds.make_embed(
+            ctx=ctx, author=True, title="Results of vote", 
+            description=f"""<:yes:914162499023142996> - **{len(yes_users)}** {" ".join(yes_users)}
+            <:no:914162576403873832> - **{len(no_users)}** {" ".join(no_users)}
+            **Both: ** **{len(both_users)}** {" ".join(both_users)}
+            **Did not vote: ** **{len(skipped_users)}** {" ".join(skipped_users)}
+            """, color=discord.Color.green()
+        )
+
+        await ctx.send_followup(embed=embed)
 
 
 def setup(bot: commands.Bot) -> None:
     bot.add_cog(GeneralCommands(bot))
     log.info("Commands loaded: general")
+
