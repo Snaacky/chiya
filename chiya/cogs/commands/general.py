ec142e712405fe92dd37cfe5da8e38665c60b470
REDACTED <vikee.email@gmail.com>
2022-04-13 16:33:04 +0530

    Replace hardcoded IDs with config variables.

--- chiya/cogs/commands/general.py
+++ chiya/cogs/commands/general.py
@@ -1,13 +1,10 @@
 import logging
-from unittest import skip
 
 import discord
-from discord.commands import Option, context, permissions, slash_command
-from discord.ext import commands
-
 from chiya import config
 from chiya.utils import embeds
-
+from discord.commands import Option, context, permissions, slash_command
+from discord.ext import commands
 
 log = logging.getLogger(__name__)
 
@@ -131,11 +128,11 @@ class GeneralCommands(commands.Cog):
 
         yes_reactions = None
         no_reactions = None
-        
+
         for reaction in message.reactions:
-            if reaction.emoji.id == config['emoji']['yes']:
+            if reaction.emoji.id == config["emoji"]["yes"]:
                 yes_reactions = reaction
-            if reaction.emoji.id == config['emoji']['no']:
+            if reaction.emoji.id == config["emoji"]["no"]:
                 no_reactions = reaction
 
         if not yes_reactions or not no_reactions:
@@ -147,27 +144,34 @@ class GeneralCommands(commands.Cog):
         yes_users = set(await yes_reactions.users().flatten())
         no_users = set(await no_reactions.users().flatten())
         both_users = yes_users.intersection(no_users)
-        role_staff = discord.utils.get(message.guild.roles, id=config['roles']['staff'])
+        role_staff = discord.utils.get(message.guild.roles, id=config["roles"]["staff"])
         staff_users = set(user for user in role_staff.members)
         skipped_users = staff_users.difference(yes_users.union(no_users))
-        
+
         yes_users = [user.mention if not user.bot else "" for user in yes_users.copy()]
         no_users = [user.mention if not user.bot else "" for user in no_users.copy()]
-        both_users = [user.mention if not user.bot else "" for user in both_users.copy()]
-        skipped_users = [user.mention if not user.bot else "" for user in skipped_users.copy()]
-        
+        both_users = [
+            user.mention if not user.bot else "" for user in both_users.copy()
+        ]
+        skipped_users = [
+            user.mention if not user.bot else "" for user in skipped_users.copy()
+        ]
+
         embed = embeds.make_embed(
-            ctx=ctx, author=True, title="Results of vote", 
-            description=f"""<:yes:914162499023142996> - **{len(yes_users) - 1}** {" ".join(yes_users)}
-            <:no:914162576403873832> - **{len(no_users) - 1}** {" ".join(no_users)}
+            ctx=ctx,
+            author=True,
+            title="Results of vote",
+            description=f"""<:yes:{config['emoji']['yes']}> - **{len(yes_users) - 1}** {" ".join(yes_users)}
+            <:no:{config['emoji']['no']}> - **{len(no_users) - 1}** {" ".join(no_users)}
             **Both: ** **{len(both_users) - 1}** {" ".join(both_users)}
             **Did not vote: ** **{len(skipped_users)}** {" ".join(skipped_users)}
-            """, color=discord.Color.green()
+            """,
+            color=discord.Color.green(),
         )
 
         await ctx.send_followup(embed=embed)
 
+
 def setup(bot: commands.Bot) -> None:
     bot.add_cog(GeneralCommands(bot))
     log.info("Commands loaded: general")
-
