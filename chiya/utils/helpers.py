f86f4dec5254b58d86a872c1b3fdec94d58e6652
lunarmint <developer@chocomint.dev>
2022-06-14 23:17:17 +0700

    Added comments

--- chiya/utils/helpers.py
+++ chiya/utils/helpers.py
@@ -11,15 +11,19 @@ log = logging.getLogger(__name__)
 
 
 async def can_action_member(ctx: context.ApplicationContext, member: discord.Member) -> bool:
+    # Stop mods from actioning on the bot.
     if member.bot:
         return False
 
+    # Checking if bot is able to perform the action.
     if member.top_role >= member.guild.me.top_role:
         return False
 
+    # Allow owner to override all limitations.
     if member.id == ctx.guild.owner_id:
         return True
 
+    # Prevents mods from actioning other mods.
     if ctx.author.top_role <= member.top_role:
         return False
 
@@ -51,14 +55,17 @@ def get_duration(duration) -> Tuple[str, int]:
         seconds=match_list[7],
     )
 
+    # String that will store the duration in a more digestible format.
     duration_string = ""
     for time_unit in duration:
+        # If the time value is declared, set it to float type for timedelta object compatibility. 0 otherwise.
         duration[time_unit] = float(duration[time_unit]) if duration[time_unit] != "" else 0
 
+        # Prevent timedelta object from raising overflow exception from very large values.
         if duration[time_unit] > 999:
             duration[time_unit] = 999
 
-        # If the time value is 1, make the time unit into singular form and plural otherwise
+        # If the time value is 1, make the time unit into singular form and plural otherwise.
         if duration[time_unit] == 0:
             continue
         elif duration[time_unit] == 1:
@@ -66,6 +73,7 @@ def get_duration(duration) -> Tuple[str, int]:
         else:
             duration_string += f"{int(duration[time_unit])} {time_unit} "
 
+    # Converting 1 year = 365 days and 1 month = 30 days since they're not natively supported.
     duration["days"] += duration["years"] * 365 + duration["months"] * 30
 
     time_delta = datetime.timedelta(
