acbbc114ebd7676006d1c03ec1b0a1b271c5a4c5
Jimbo <17926797+Kapppa@users.noreply.github.com>
2022-08-03 21:22:37 +0100

    Improved new implementation of TrackerStatus (#196)
    Co-authored-by: Snaacky <snaacky@pm.me>

--- chiya/cogs/commands/trackerstatus.py
+++ chiya/cogs/commands/trackerstatus.py
@@ -1,26 +1,33 @@
 import logging
 
-import requests
 from discord.commands import slash_command, context, Option
 from discord.ext import commands, tasks
 
 from chiya import config
-from chiya.utils import embeds
+from chiya.utils.trackerstatus import TrackerStatus, TrackerStatusAB, TrackerStatusInfo, TrackerStatusMAM
 
 
 log = logging.getLogger(__name__)
-trackers = ["AR", "BTN", "GGn", "PTP", "RED", "OPS"]
+
+trackers: list[TrackerStatus] = [
+    TrackerStatusInfo("AR"),
+    TrackerStatusInfo("BTN"),
+    TrackerStatusInfo("GGn"),
+    TrackerStatusInfo("PTP"),
+    TrackerStatusInfo("RED"),
+    TrackerStatusInfo("OPS"),
+    TrackerStatusAB(),
+    TrackerStatusMAM()
+]
+trackers_dict = {item.tracker: item for item in trackers}
 
 
 class TrackerStatusCommands(commands.Cog):
     # TODO: Add support for trackers that offer their own status page.
-    # https://status.animebytes.tv/
     # http://about.empornium.ph/
-    # https://status.myanonamouse.net/
     # http://is.morethantv.online/
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-        self.cache = {}
         self.refresh_data.start()
 
     def cog_unload(self) -> None:
@@ -33,48 +40,25 @@ class TrackerStatusCommands(commands.Cog):
         every 60 seconds, respecting API limits.
         """
         for tracker in trackers:
-            try:
-                r = requests.get(url=f"https://{tracker}.trackerstatus.info/api/status/")
-                r.raise_for_status()
-            except (requests.exceptions.RequestException, requests.exceptions.HTTPError) as e:
-                log.error(e)
-                pass
-
-            if r.status_code == 200:
-                self.cache[tracker] = r.json()
-
-    def normalize_value(self, value):
-        """
-        Converts API data values into user-friendly text with status availability icon.
-        """
-        match value:
-            case "1":
-                return "<:status_online:596576749790429200> Online"
-            case "2":
-                return "<:status_dnd:596576774364856321> Unstable"
-            case "0":
-                return "<:status_offline:596576752013279242> Offline"
+            tracker.do_refresh()
 
     @slash_command(guild_ids=config["guild_ids"], description="Get tracker uptime statuses")
     async def trackerstatus(
         self,
         ctx: context.ApplicationContext,
-        tracker: Option(str, description="Tracker to get uptime statuses for", choices=trackers, required=True),
+        tracker: Option(
+            str,
+            description="Tracker to get uptime statuses for",
+            choices=sorted(list(trackers_dict.keys())),
+            required=True
+        ),
     ) -> None:
         # TODO: Change the color of the embed to green if all services are online,
         # yellow if one of the services is offline, and grey or red if all are offline.
         await ctx.defer()
 
-        embed = embeds.make_embed(
-            ctx=ctx,
-            title=f"Tracker Status: {tracker}",
-        )
-
-        for key, value in self.cache[tracker].items():
-            # Skip over any keys that we don't want to return in the embed.
-            if key in ["tweet", "TrackerHTTPAddresses", "TrackerHTTPSAddresses"]:
-                continue
-            embed.add_field(name=key, value=self.normalize_value(value), inline=True)
+        tracker: TrackerStatus = trackers_dict.get(tracker)
+        embed = tracker.get_status_embed(ctx)
 
         await ctx.send_followup(embed=embed)
 
---
+++ chiya/utils/trackerstatus.py
@@ -0,0 +1,177 @@
+import logging
+
+import discord
+import requests
+from discord.commands import context
+
+from chiya.utils import embeds
+
+
+log = logging.getLogger(__name__)
+
+
+class TrackerStatus():
+    def __init__(self, tracker: str) -> None:
+        self.tracker = tracker
+        self.cache_data = None
+
+    def get_status_embed(self, ctx: context.ApplicationContext = None) -> discord.Embed:
+        pass
+
+    def do_refresh(self) -> None:
+        pass
+
+
+class TrackerStatusInfo(TrackerStatus):
+    """
+    Gets status of a tracker from trackerstatus.info
+    """
+    def __init__(self, tracker: str) -> None:
+        super().__init__(tracker)
+
+    def get_status_embed(self, ctx: context.ApplicationContext = None) -> discord.Embed:
+        embed = embeds.make_embed(
+            ctx=ctx,
+            title=f"Tracker Status: {self.tracker}",
+        )
+
+        if self.cache_data is None:
+            self.do_refresh()
+
+        for key, value in self.cache_data.items():
+            # Skip over any keys that we don't want to return in the embed.
+            if key in ["tweet", "TrackerHTTPAddresses", "TrackerHTTPSAddresses"]:
+                continue
+            embed.add_field(name=key, value=self.normalize_value(value), inline=True)
+
+        return embed
+
+    def normalize_value(self, value):
+        """
+        Converts API data values into user-friendly text with status availability icon.
+        """
+        match value:
+            case "1":
+                return "<:status_online:596576749790429200> Online"
+            case "2":
+                return "<:status_dnd:596576774364856321> Unstable"
+            case "0":
+                return "<:status_offline:596576752013279242> Offline"
+
+    def do_refresh(self) -> None:
+        try:
+            r = requests.get(url=f"https://{self.tracker}.trackerstatus.info/api/status/")
+            r.raise_for_status()
+        except (requests.exceptions.RequestException, requests.exceptions.HTTPError) as e:
+            log.error(e)
+            pass
+
+        if r.status_code == 200:
+            self.cache_data = r.json()
+
+
+class TrackerStatusAB(TrackerStatus):
+    """
+    Gets status of AB from API
+    """
+    URL = "https://status.animebytes.tv/api/status"
+
+    def __init__(self) -> None:
+        super().__init__("AB")
+
+    def get_status_embed(self, ctx: context.ApplicationContext = None) -> discord.Embed:
+        embed = embeds.make_embed(
+            ctx=ctx,
+            title=f"Tracker Status: {self.tracker}",
+        )
+
+        if self.cache_data is None:
+            self.do_refresh()
+
+        if not self.cache_data.get("status", False):
+            embed.set_footer("<:status_offline:596576752013279242> API Failed")
+
+        for key, value in self.cache_data.get("status", {}).items():
+            embed.add_field(name=key, value=self.normalize_value(value.get("status")), inline=True)
+
+        return embed
+
+    def normalize_value(self, value):
+        """
+        Converts API data values into user-friendly text with status availability icon.
+        """
+        match value:
+            case 1:
+                return "<:status_online:596576749790429200> Online"
+            case 2:
+                return "<:status_dnd:596576774364856321> Unstable"
+            case 0:
+                return "<:status_offline:596576752013279242> Offline"
+
+    def do_refresh(self) -> None:
+        try:
+            r = requests.get(url=self.URL)
+            r.raise_for_status()
+        except (requests.exceptions.RequestException, requests.exceptions.HTTPError) as e:
+            log.error(e)
+            pass
+
+        if r.status_code == 200:
+            self.cache_data: dict = r.json()
+
+
+class TrackerStatusUptimeRobot(TrackerStatus):
+    """
+    Gets status of a tracker from trackerstatus.info
+    """
+    def __init__(self, tracker: str, url: str) -> None:
+        self.url = url
+
+        super().__init__(tracker)
+
+    def get_status_embed(self, ctx: context.ApplicationContext = None) -> discord.Embed:
+        embed = embeds.make_embed(
+            ctx=ctx,
+            title=f"Tracker Status: {self.tracker}",
+        )
+
+        if self.cache_data is None:
+            self.do_refresh()
+
+        monitors: list[dict] = self.cache_data.get("psp", {}).get("monitors", [])
+
+        for monitor in monitors:
+            dratio: dict = monitor.get("dailyRatios", [])[0]
+            embed.add_field(name=monitor.get("name", "UNKNOWN"), value=self.normalize_value(dratio), inline=True)
+
+        return embed
+
+    def normalize_value(self, value: dict):
+        """
+        Converts API data values into user-friendly text with status availability icon.
+        """
+        if value.get("label") == "success":
+            return "<:status_online:596576749790429200> Online"
+        ratio = float(value.get("ratio", "0"))
+        if float(value.get("ratio")) > 95:
+            return "<:status_dnd:596576774364856321> Unstable"
+        elif ratio > 0:
+            return "<:status_offline:596576752013279242> Offline"
+
+        return "<:status_offline:596576752013279242> Unknown"
+
+    def do_refresh(self) -> None:
+        try:
+            r = requests.get(url=self.url)
+            r.raise_for_status()
+        except (requests.exceptions.RequestException, requests.exceptions.HTTPError) as e:
+            log.error(e)
+            pass
+
+        if r.status_code == 200:
+            self.cache_data: dict = r.json()
+
+
+class TrackerStatusMAM(TrackerStatusUptimeRobot):
+    def __init__(self) -> None:
+        super().__init__("MAM", "https://status.myanonamouse.net/api/getMonitorList/vl59BTEJX")
