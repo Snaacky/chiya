7f791311ec3b3d19fd51be6ef2aace0c1ff762d0
Snaacky <snaacky@pm.me>
2022-08-18 10:15:04 -0400

    Ported /purge to discord.py
    - Replaced pycord import with discord.py equivalent.
    - Fixed command and parameter definitions.
    - Fixed all occurrences of context type hinting.
    - Fixed all occurrences of response defering.
    - Replaced ctx.author occurrences with ctx.user equivalent.
    - Fixed all occurrences of ctx followups.
    - Made setup function async and now awaiting bot.add_cog()

--- chiya/cogs/commands/purge.py
+++ chiya/cogs/commands/purge.py
@@ -1,7 +1,7 @@
 import logging
 
 import discord
-from discord.commands import Option, context, slash_command
+from discord import app_commands
 from discord.ext import commands
 
 from chiya import config
@@ -15,13 +15,13 @@ class PurgeCommands(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    async def can_purge_messages(self, ctx: context.ApplicationContext) -> bool:
+    async def can_purge_messages(self, ctx: discord.Interaction) -> bool:
         """
         Check used by purge function to make sure that the moderation,
         development, logs, and tickets categories can't be purged for
         security reasons.
         """
-        if ctx.author.id == ctx.guild.owner.id:
+        if ctx.user.id == ctx.guild.owner.id:
             return True
 
         if ctx.channel.category_id in [
@@ -34,14 +34,13 @@ class PurgeCommands(commands.Cog):
 
         return True
 
-    @slash_command(guild_ids=config["guild_ids"], description="Purge the last X amount of messages")
-    @commands.has_role(config["roles"]["staff"])
-    async def purge(
-        self,
-        ctx: context.ApplicationContext,
-        amount: Option(int, description="The amount of messages to be purged", required=True),
-        reason: Option(str, description="The reason why the messages are being purged", required=True),
-    ) -> None:
+    @app_commands.command(name="purge", description="Purge the last X amount of messages")
+    @app_commands.guilds(config["guild_id"])
+    @app_commands.guild_only()
+    @app_commands.checks.has_role(config["roles"]["staff"])
+    @app_commands.describe(amount="The amount of messages to be purged")
+    @app_commands.describe(reason="The reason why the messages are being purged")
+    async def purge(self, ctx: discord.Interaction, amount: int, reason: str) -> None:
         """
         Removes the last X amount of messages in bulk.
 
@@ -51,7 +50,7 @@ class PurgeCommands(commands.Cog):
         Cannot be used in the moderation, development, logs, or archive
         categories for security reasons.
         """
-        await ctx.defer()
+        await ctx.response.defer(thinking=True)
 
         if not await self.can_purge_messages(ctx):
             return await embeds.error_message(ctx=ctx, description="You cannot use that command in this category.")
@@ -63,15 +62,15 @@ class PurgeCommands(commands.Cog):
 
         embed = embeds.make_embed(
             title="Purged messages",
-            description=f"{ctx.author.mention} purged {amount} {'message' if amount == 1 else 'messages'}.",
+            description=f"{ctx.user.mention} purged {amount} {'message' if amount == 1 else 'messages'}.",
             thumbnail_url="https://i.imgur.com/EDy6jCp.png",
             color=discord.Color.red(),
             fields=[{"name": "Reason:", "value": reason, "inline": False}],
         )
         await ctx.channel.purge(limit=amount, before=ctx.channel.last_message.created_at, bulk=True)
-        await ctx.send_followup(embed=embed)
+        await ctx.followup.send(embed=embed)
 
 
-def setup(bot: commands.Bot) -> None:
-    bot.add_cog(PurgeCommands(bot))
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(PurgeCommands(bot))
     log.info("Commands loaded: purge")
