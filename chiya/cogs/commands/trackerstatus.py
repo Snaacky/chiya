f346e76b81f13154d32e9ec9f9644da68362b664
Jimbo <17926797+Kapppa@users.noreply.github.com>
2022-08-18 17:35:10 +0100

    Port trackerstatus
    Support AutoComplete

--- chiya/cogs/commands/trackerstatus.py
+++ chiya/cogs/commands/trackerstatus.py
@@ -1,6 +1,8 @@
 import logging
+from typing import List
 
-from discord.commands import slash_command, context, Option
+import discord
+from discord import app_commands
 from discord.ext import commands, tasks
 
 from chiya import config
@@ -20,7 +22,7 @@ trackers: list[TrackerStatus] = [
     TrackerStatusMAM()
 ]
 trackers_dict = {item.tracker: item for item in trackers}
-
+trackers_list = sorted(list(trackers_dict.keys()))
 
 class TrackerStatusCommands(commands.Cog):
     # TODO: Add support for trackers that offer their own status page.
@@ -42,27 +44,35 @@ class TrackerStatusCommands(commands.Cog):
         for tracker in trackers:
             tracker.do_refresh()
 
-    @slash_command(guild_ids=config["guild_ids"], description="Get tracker uptime statuses")
+    async def tracker_autocomplete(
+        interaction: discord.Interaction,
+        current: str,
+    ) -> List[app_commands.Choice[str]]:
+        return [
+            app_commands.Choice(name=tracker, value=tracker)
+            for tracker in trackers_list
+        ]
+
+
+    @app_commands.command(name="trackerstatus", description="Get tracker uptime statuses")
+    @app_commands.guilds(config["guild_id"])
+    @app_commands.describe(tracker="Tracker to get uptime statuses for")
+    @app_commands.autocomplete(tracker=tracker_autocomplete)
     async def trackerstatus(
         self,
-        ctx: context.ApplicationContext,
-        tracker: Option(
-            str,
-            description="Tracker to get uptime statuses for",
-            choices=sorted(list(trackers_dict.keys())),
-            required=True
-        ),
+        ctx: discord.Interaction,
+        tracker: str,
     ) -> None:
         # TODO: Change the color of the embed to green if all services are online,
         # yellow if one of the services is offline, and grey or red if all are offline.
-        await ctx.defer()
+        await ctx.response.defer()
 
         tracker: TrackerStatus = trackers_dict.get(tracker)
         embed = tracker.get_status_embed(ctx)
 
-        await ctx.send_followup(embed=embed)
+        await ctx.followup.send(embed=embed)
 
 
-def setup(bot: commands.Bot) -> None:
-    bot.add_cog(TrackerStatusCommands(bot))
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(TrackerStatusCommands(bot))
     log.info("Commands loaded: trackerstatus")
