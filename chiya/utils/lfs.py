0ebbd68878b05b91402f83d9223656618bf86b08
qLunar <100940270+qLunar@users.noreply.github.com>
2023-10-02 18:05:01 +0530

    feat: add function get_image to fetch git-lfs urls

---
+++ chiya/utils/lfs.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import inspect
+
+from chiya import config
+
+def get_image(imgname: str) -> str:
+    """
+    Takes the name of the image and grabs the filename of the function using the inspect module.
+        Args: 
+            imgname: The name of the image file, including the file extension.
+        Returns: 
+            The URL of the image file.
+    """
+    base_url = config["lfs-url"]
+    full_path = inspect.stack()[1].filename
+    filename = Path(full_path).stem 
+    image_url = f"{base_url}/{filename}/{imgname}"
+    return image_url
