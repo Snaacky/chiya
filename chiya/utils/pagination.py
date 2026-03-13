ef30c757b1cb0e6fdce78534d7ba2a8e3bc80e22
Snaacky <snaacky@pm.me>
2026-03-13 08:06:20 -0400

    fix: add spacing between /search actions

--- chiya/utils/pagination.py
+++ chiya/utils/pagination.py
@@ -107,7 +107,7 @@ class MySource(menus.ListPageSource):
 
     async def format_page(self, menu, page) -> discord.Embed:
         page_info = await self.get_page(menu.current_page)
-        desc = "\n".join(page_info)
+        desc = "\n\n".join(page_info)
         self.embed.description = desc
         self.embed.set_footer(text=f"Page {menu.current_page + 1}/{self.get_max_pages()}")
         return self.embed
