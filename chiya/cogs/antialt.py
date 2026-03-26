a7cf249797e4a2d468a71050732fc106d52db850
Snaacky <snaacky@pm.me>
2026-03-26 12:31:02 -0400

    feat: add anti-alt/bot cog

---
+++ chiya/cogs/antialt.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timedelta, timezone
+
+from discord.ext import commands
+
+from chiya.utils import embeds
+
+
+class AntiAltCog(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member) -> None:
+        if datetime.now(timezone.utc) - member.created_at < timedelta(days=183):
+
+            user_embed = embeds.make_embed(
+                title="Uh-oh, you've been kicked!",
+                image_url="https://files.catbox.moe/vp5op4.gif",
+                color=discord.Color.blurple(),
+                fields=[
+                    {"name": "Server:", "value": f"{ctx.guild.name}", "inline": True},
+                    {"name": "Reason:", "value": "We do not allow new Discord accounts to participate in our community to avoid bots and alt accounts. Please switch to your main account or try again at a later date.", "inline": False},
+                ],
+            )
+
+            try:
+                await member.send(embed=user_embed)
+            except (discord.Forbidden, discord.HTTPException):
+                pass
+
+            await member.kick(reason="Account younger than 6 months")
+
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(AntiAltCog(bot))
