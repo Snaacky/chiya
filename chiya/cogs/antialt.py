15e96bd79512f992d14c955e7ad3cf64b1280f81
Snaacky <snaacky@pm.me>
2026-03-26 12:37:17 -0400

    fix: ruff formatting

--- chiya/cogs/antialt.py
+++ chiya/cogs/antialt.py
@@ -13,14 +13,17 @@ class AntiAltCog(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member) -> None:
         if datetime.now(timezone.utc) - member.created_at < timedelta(days=183):
-
             user_embed = embeds.make_embed(
                 title="Uh-oh, you've been kicked!",
                 image_url="https://files.catbox.moe/vp5op4.gif",
                 color=discord.Color.blurple(),
                 fields=[
                     {"name": "Server:", "value": f"{member.guild.name}", "inline": True},
-                    {"name": "Reason:", "value": "We do not allow new Discord accounts to participate in our community to avoid bots and alt accounts. Please switch to your main account or try again at a later date.", "inline": False},
+                    {
+                        "name": "Reason:",
+                        "value": "We do not allow new Discord accounts to participate in our community to avoid bots and alt accounts. Please switch to your main account or try again at a later date.",
+                        "inline": False,
+                    },
                 ],
             )
 
